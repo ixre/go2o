aeb56c7435183ead3f01987e3193bb09ac624d92
jarrysix <jarrysix@gmail.com>
2015-05-03 18:20:58 +0800

    commit

---
+++ src/core/types.go
@@ -0,0 +1,21 @@
+/**
+ * Copyright 2015 @ S1N1 Team.
+ * name : types.go
+ * author : jarryliu
+ * date : -- :
+ * description :
+ * history :
+ */
+package core
+
+import (
+    "encoding/gob"
+    "go2o/src/core/domain/interface/member"
+    "go2o/src/core/domain/interface/partner"
+)
+
+// 注册序列号类型
+func RegisterTypes(){
+    gob.Register(&member.ValueMember{})
+    gob.Register(&partner.ValuePartner{})
+}
