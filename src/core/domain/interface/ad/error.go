ac6a187f5fca41cf90f6ac0aaa74578f60543777
jarryliu <jarrysix@gmail.com>
2015-06-26 23:33:26 +0800

    ad manager

---
+++ src/core/domain/interface/ad/error.go
@@ -0,0 +1,19 @@
+/**
+ * Copyright 2014 @ S1N1 Team.
+ * name :
+ * author : jarryliu
+ * date : 2013-12-12 16:53
+ * description :
+ * history :
+ */
+
+package ad
+
+import (
+	"go2o/src/core/infrastructure/domain"
+)
+
+var (
+	ErrNameExists *domain.DomainError = domain.NewDomainError(
+		"name_exists", "已经存在相同的名称的广告")
+)
