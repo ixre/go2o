053babca5bc7e9e3554f385a15b5993372be0feb
jarryliu <jarrysix@gmail.com>
2015-07-27 09:11:54 +0800

    mss rep

---
+++ src/core/domain/interface/partner/mss/mss_rep.go
@@ -0,0 +1,18 @@
+/**
+ * Copyright 2015 @ S1N1 Team.
+ * name : mss_rep
+ * author : jarryliu
+ * date : 2015-07-27 08:46
+ * description :
+ * history :
+ */
+package mss
+
+type IMssRep interface {
+	// 获取邮箱模板
+	GetMailTemplate(partnerId, id int) *MailTemplate
+	// 保存邮箱模版
+	SaveMailTemplate(*MailTemplate) (int, error)
+	// 获取所有的邮箱模版
+	GetMailTemplates(partnerId int) []*MailTemplate
+}
---
+++ src/core/repository/mss_rep.go
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2015 @ S1N1 Team.
+ * name : mss_rep
+ * author : jarryliu
+ * date : 2015-07-27 09:03
+ * description :
+ * history :
+ */
+package repository
+
+import (
+	"github.com/atnet/gof/db"
+	"go2o/src/core/domain/interface/partner/mss"
+)
+
+var _ mss.IMssRep = new(MssRep)
+
+type MssRep struct {
+	_conn db.Connector
+}
+
+func NewMssRep(conn db.Connector) mss.IMssRep {
+	return &MssRep{
+		_conn: conn,
+	}
+}
+
+// 获取邮箱模板
+func (this *MssRep) GetMailTemplate(partnerId, id int) *mss.MailTemplate {
+	var e mss.MailTemplate
+	if err := this._conn.GetOrm().Get(id, &e); err == nil {
+		return &e
+	}
+	return nil
+}
+
+// 保存邮箱模版
+func (this *MssRep) SaveMailTemplate(v *mss.MailTemplate) (int, error) {
+	var err error
+	var orm = this._conn.GetOrm()
+	if v.Id > 0 {
+		_, _, err = orm.Save(v.Id, v)
+	} else {
+		_, _, err = orm.Save(nil, v)
+		this._conn.ExecScalar("SELECT MAX(id) FROM pt_mail_template WHERE partner_id=?", &v.Id, v.PartnerId)
+	}
+	return v.Id, err
+}
+
+// 获取所有的邮箱模版
+func (this *MssRep) GetMailTemplates(partnerId int) []*mss.MailTemplate {
+	var list = []*mss.MailTemplate{}
+	this._conn.GetOrm().Select(&list, " partner_id=?", partnerId)
+	return list
+}
