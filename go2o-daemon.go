30f424d64f305e46bf2c087b833297ad98e7f9bb
jarryliu <jarrysix@gmail.com>
2015-08-04 20:26:58 +0800

    commit

---
+++ go2o-daemon.go
@@ -0,0 +1,14 @@
+/**
+ * Copyright 2015 @ S1N1 Team.
+ * name : daemon
+ * author : jarryliu
+ * date : 2015-08-04 20:23
+ * description :
+ * history :
+ */
+package main
+import "go2o/src/app/daemon"
+
+func main(){
+	daemon.Run(nil)
+}
