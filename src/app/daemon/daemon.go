aa591b9156179a6c48f7f25a8d83a5a6f95aec3e
jarryliu <jarrysix@gmail.com>
2015-08-06 11:13:00 +0800

    commit

--- src/app/daemon/daemon.go
+++ src/app/daemon/daemon.go
@@ -14,10 +14,16 @@ import (
 	"fmt"
 	"github.com/atnet/gof"
 	"go2o/src/core"
+	"strings"
 )
 
+const(
+	ServeMail string = "mail"
+	ServeOrder string ="order"
+)
 var (
 	appCtx *core.MainApp
+	allService = []string{ServeMail,ServeOrder}
 )
 
 // 运行
@@ -27,22 +33,51 @@ func Run(ctx gof.App) {
 	} else {
 		appCtx = getAppCtx("app.conf")
 	}
-
-	fmt.Println("[ Go2o][ Daemon][ Booted] - Daemon service is running.")
-	//daemon()
-	startMailQueue()
+	bootService(allService)
 }
 
 // 自定义参数运行
 func FlagRun() {
 	var conf string
+	var debug bool
+	var trace bool
+	var service string
+	var serviceArr []string
 	flag.StringVar(&conf, "conf", "app.conf", "")
-	Run(getAppCtx(conf))
+	flag.BoolVar(&debug,"debug",true,"")
+	flag.BoolVar(&trace,"trace",true,"")
+	flag.StringVar(&service,"service",strings.Join(allService,","),"")
+
+	flag.Parse()
+
+	appCtx = getAppCtx(conf)
+	appCtx.Init(debug,trace)
+	if service == "all" {
+		serviceArr = allService
+	}else {
+		serviceArr = strings.Split(service, ",")
+	}
+
+	bootService(serviceArr)
 }
 
 func getAppCtx(conf string) *core.MainApp {
 	return core.NewMainApp(conf)
 }
+
+func bootService(arr []string){
+	fmt.Println("[ Go2o][ Daemon][ Booted] - Daemon service is running.")
+	for _,v := range arr {
+		switch v {
+		case ServeMail:
+			fmt.Println("[ Go2o][ Daemon][ Booted] - mail daemon running")
+			go startMailQueue()
+		case ServeOrder:
+			fmt.Println("[ Go2o][ Daemon][ Booted] - order daemon running")
+		}
+	}
+}
+
 
 func daemon() {
 	go partnerDaemon()
