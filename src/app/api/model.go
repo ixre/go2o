6ac288d28ed128744ac509d883cefd23396047cd
jarryliu <jarrysix@gmail.com>
2015-10-31 02:03:24 +0800

    commit

---
+++ src/app/api/model.go
@@ -0,0 +1,16 @@
+/**
+ * Copyright 2015 @ z3q.net.
+ * name : model
+ * author : jarryliu
+ * date : 2015-10-31 00:35
+ * description :
+ * history :
+ */
+package api
+
+type(
+	AsyncResult struct{
+		MemberUpdated bool			//会员已经更新
+		AccountUpdated bool			//会员账户已经更新
+	}
+)
