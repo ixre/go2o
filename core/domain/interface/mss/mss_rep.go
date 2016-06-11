7ff166e154bcd15adf3556f4c813add9924c9221
jarryliu <jarrysix@gmail.com>
2016-06-12 07:12:01 +0800

    mss refactor

--- core/domain/interface/mss/mss_rep.go
+++ core/domain/interface/mss/mss_rep.go
@@ -10,14 +10,21 @@ package mss
 
 type IMssRep interface {
 	GetManager() IMessageProvider
+
+	// 获取消息设置
+	GetConfig(userId int) *Config
+
+	// 保存消息设置
+	SaveConfig(userId int, conf *Config) error
+
 	// 获取邮箱模板
-	GetMailTemplate(merchantId, id int) *MailTemplate
+	GetMailTemplate(userId, id int) *MailTemplate
 	// 保存邮箱模版
 	SaveMailTemplate(*MailTemplate) (int, error)
 	// 获取所有的邮箱模版
-	GetMailTemplates(merchantId int) []*MailTemplate
+	GetMailTemplates(userId int) []*MailTemplate
 	// 删除邮件模板
-	DeleteMailTemplate(merchantId, id int) error
+	DeleteMailTemplate(userId, id int) error
 	// 加入到发送对列
 	JoinMailTaskToQueen(*MailTask) error
 }
