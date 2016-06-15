7c403f42b2bf148a6b35bd1c07de4cf34d2f8a15
jarryliu <jarrysix@gmail.com>
2016-06-15 23:15:26 +0800

    rename
    Former-commit-id: ecc9053e7633393dd0d136d53b796361708a3e34

---
+++ app/restapi/merchant_c.go
@@ -0,0 +1,30 @@
+/**
+ * Copyright 2015 @ z3q.net.
+ * name : partner_c.go
+ * author : jarryliu
+ * date : -- :
+ * description :
+ * history :
+ */
+package restapi
+
+import (
+	"github.com/jsix/gof"
+	"go2o/core/service/dps"
+	"gopkg.in/labstack/echo.v1"
+	"net/http"
+)
+
+type partnerC struct {
+}
+
+// 获取广告数据
+func (this *partnerC) Get_ad(ctx *echo.Context) error {
+	merchantId := getMerchantId(ctx)
+	adName := ctx.Request().FormValue("ad_name")
+	dto := dps.AdService.GetAdAndDataByKey(merchantId, adName)
+	if dto != nil {
+		return ctx.JSON(http.StatusOK, dto)
+	}
+	return ctx.JSON(http.StatusOK, gof.Message{Message: "没有广告数据"})
+}
