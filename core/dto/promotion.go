0c643725ed47fda1091511a78b933815b4920f08
jarryliu <jarrysix@gmail.com>
2016-06-24 22:12:23 +0800

    coupon dto
    Former-commit-id: 8552b55b14cce68629c1a2c34964fbe5c5fdfa1a

--- core/dto/promotion.go
+++ core/dto/promotion.go
@@ -9,40 +9,29 @@
 package dto
 
 type (
+	//member_rep LINE:562
 	ValueCoupon struct {
 		// 优惠券编号
-		Id int `db:"id" pk:"yes"`
+		Id int `db:"id"`
+
+		Num int `db:"num"`
+
+		// 优惠券标题
+		Title string `db:"title"`
 
 		//优惠码
 		Code string `db:"code"`
 
-		// 优惠码可用数量
-		Amount int `db:"amount"`
-
-		// 优惠码数量
-		TotalAmount int `db:"total_amount"`
-
 		//优惠金额
 		Fee int `db:"fee"`
-
-		//赠送积分
-		Integral int `db:"integral"`
 
 		//订单折扣(不打折为100)
 		Discount int `db:"discount"`
 
-		//等级限制
-		MinLevel int `db:"min_level"`
+		//是否使用
+		IsUsed int `db:"is_used"`
 
-		//订单金额限制
-		MinFee int `db:"min_fee"`
-
-		BeginTime int64 `db:"begin_time"`
-		OverTime  int64 `db:"over_time"`
-
-		//是否需要绑定。反之可以直接使用
-		NeedBind int `db:"need_bind"`
-
-		CreateTime int64 `db:"create_time"`
+		//结束日期
+		OverTime int64 `db:"over_time"`
 	}
 )
