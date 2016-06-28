0d0a66fa01e85f2f5e7ad18b0cf07143a3372d06
jarryliu <jarrysix@gmail.com>
2016-06-28 09:46:40 +0800

    ad refactor
    Former-commit-id: 054031793d671e002b1561bf9fba4689d23b4044

--- core/domain/interface/ad/ad.go
+++ core/domain/interface/ad/ad.go
@@ -155,7 +155,7 @@ type (
 		Name string `db:"name"`
 		//todo:广告位类型限制
 		// 广告类型限制,0为无限制
-		TypeLimit int //`db:"type_limit"`
+		TypeLimit int `db:"-"` //`db:"type_limit"`
 		// 是否开放给外部
 		Opened int `db:"opened"`
 		// 是否启用
