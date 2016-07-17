9fbba633ffce94d60741a210f641eaf035ca5b85
jarryliu <jarrysix@gmail.com>
2016-07-17 17:29:03 +0800

    after sales order
    Former-commit-id: 3a00964167449be5b45d8617dd21fe7dc7a586a2

--- core/domain/after-sales/after_sales.go
+++ core/domain/after-sales/after_sales.go
@@ -7,3 +7,184 @@
  * history :
  */
 package afterSales
+
+import (
+	"errors"
+	"github.com/jsix/gof/db/orm"
+	"go2o/core/domain/interface/after-sales"
+	"go2o/core/domain/interface/order"
+	"go2o/core/domain/tmp"
+	"time"
+)
+
+var _ afterSales.IAfterSalesOrder = new(afterSalesOrderImpl)
+
+type afterSalesOrderImpl struct {
+	_value    *afterSales.AfterSalesOrder
+	_rep      afterSales.IAfterSalesRep
+	_order    order.ISubOrder
+	_orderRep order.IOrderRep
+}
+
+func newAfterSalesOrder(v *afterSales.AfterSalesOrder,
+	rep afterSales.IAfterSalesRep, orderRep order.IOrderRep) *afterSalesOrderImpl {
+	return &afterSalesOrderImpl{
+		_value:    v,
+		_rep:      rep,
+		_orderRep: orderRep,
+	}
+}
+
+// 获取领域编号
+func (a *afterSalesOrderImpl) GetDomainId() int {
+	return a._value.Id
+}
+
+func (a *afterSalesOrderImpl) SaveOrder() error {
+	if a._value.OrderId <= 0 {
+		panic(errors.New("售后单没有绑定订单"))
+	}
+	if a._value.ItemId <= 0 || a._value.Quantity <= 0 {
+		panic(errors.New("售后单缺少商品"))
+	}
+	a._value.UpdateTime = time.Now().Unix()
+	id, err := orm.Save(tmp.Db().GetOrm(), a._value, a.GetDomainId())
+	a._value.Id = id
+	return err
+}
+
+// 获取订单
+func (a *afterSalesOrderImpl) GetOrder() order.ISubOrder {
+	if a._order == nil {
+		if a._value.OrderId > 0 {
+			a._order = a._orderRep.Manager().GetSubOrder(a._value.OrderId)
+		}
+		if a._order == nil {
+			panic(errors.New("售后单对应的订单不存在"))
+		}
+	}
+	return a._order
+}
+
+// 设置要退回货物信息
+func (a *afterSalesOrderImpl) SetItem(itemId int, quantity int) error {
+	for _, v := range a.GetOrder().Items() {
+		if v.Id == itemId {
+			// 判断是否超过数量
+			if v.Quantity < quantity {
+				return afterSales.ErrItemOutOfQuantity
+			}
+			// 设置退回商品
+			a._value.ItemId = itemId
+			a._value.Quantity = quantity
+			return nil
+		}
+	}
+	return afterSales.ErrNoSuchOrderItem
+}
+
+// 提交售后申请
+func (a *afterSalesOrderImpl) Submit() error {
+	if a.GetDomainId() > 0 {
+		panic(errors.New("售后单已提交"))
+	}
+	if a._value.ItemId <= 0 || a._value.Quantity <= 0 {
+		panic(errors.New("售后单未包括商品项"))
+	}
+	a._value.State = afterSales.StatAwaitingVendor
+	a._value.CreateTime = time.Now().Unix()
+	return a.SaveOrder()
+}
+
+// 取消申请
+func (a *afterSalesOrderImpl) Cancel() error {
+	if a._value.State == afterSales.StatCompleted {
+		return afterSales.ErrAfterSalesOrderCompleted
+	}
+	a._value.State = afterSales.StatCancelled
+	return a.SaveOrder()
+}
+
+// 拒绝售后服务
+func (a *afterSalesOrderImpl) Decline(reason string) error {
+	if a._value.State != afterSales.StatAwaitingVendor {
+		return afterSales.ErrUnusualStat
+	}
+	a._value.State = afterSales.StatDeclined
+	a._value.VendorRemark = reason
+	return a.SaveOrder()
+}
+
+// 同意售后服务
+func (a *afterSalesOrderImpl) Agree() error {
+	if a._value.State != afterSales.StatAwaitingVendor {
+		return afterSales.ErrUnusualStat
+	}
+	// 退款,不需要退货,直接进入确认状态
+	if a._value.Type == afterSales.TypeRefund {
+		a._value.State = afterSales.StatAwaitingReturnShip
+		return a.ReturnReceive()
+	}
+	// 标记为等待收货
+	a._value.State = afterSales.StatAwaitingReturnShip
+	return a.SaveOrder()
+}
+
+// 退回商品
+func (a *afterSalesOrderImpl) ReturnShip(spName string, spOrder string, image string) error {
+	if a._value.State != afterSales.StatAwaitingReturnShip {
+		return afterSales.ErrUnusualStat
+	}
+	a._value.ReturnSpName = spName
+	a._value.ReturnSpOrder = spOrder
+	a._value.ReturnSpImage = image
+	a._value.State = afterSales.StatReturnShipped
+	return a.SaveOrder()
+}
+
+// 收货, 在商品已退回或尚未发货情况下(线下退货),可以执行此操作
+func (a *afterSalesOrderImpl) ReturnReceive() error {
+	if a._value.State != afterSales.StatAwaitingReturnShip &&
+		a._value.State != afterSales.StatReturnShipped {
+		return afterSales.ErrUnusualStat
+	}
+
+	// 判断是否需要审核
+	needConfirm := true
+	for _, v := range afterSales.IgnoreConfirmStats {
+		if a._value.State == v {
+			needConfirm = false
+			break
+		}
+	}
+	// 需要审核
+	if needConfirm {
+		a._value.State = afterSales.StatAwaitingConfirm
+		return a.SaveOrder()
+	}
+	// 不需要审核,直接完成
+	return a.Complete()
+}
+
+// 系统确认
+func (a *afterSalesOrderImpl) Confirm() error {
+	if a._value.State != afterSales.StatAwaitingConfirm {
+		return afterSales.ErrUnusualStat
+	}
+	return a.Complete()
+}
+
+// 完成执行的操作,泛化可能需要重新实现
+func (a *afterSalesOrderImpl) Complete() error {
+	a._value.State = afterSales.StatCompleted
+	return a.SaveOrder()
+}
+
+// 申请调解,只有在商户拒绝后才能申请
+func (a *afterSalesOrderImpl) RequestIntercede() error {
+	if a._value.State != afterSales.StatDeclined {
+		return afterSales.ErrUnusualStat
+	}
+	a._value.State = afterSales.StatIntercede
+	return a.SaveOrder()
+}
--- core/domain/interface/after-sales/after_sales.go
+++ core/domain/interface/after-sales/after_sales.go
@@ -8,12 +8,72 @@
  */
 package afterSales
 
+import (
+	"go2o/core/domain/interface/order"
+	"go2o/core/infrastructure/domain"
+)
+
+const (
+	// 等待商户确认
+	StatAwaitingVendor = 1 + iota
+	// 商户拒绝售后
+	StatDeclined
+	// 调解状态
+	StatIntercede
+	// 同意,等待退货
+	StatAwaitingReturnShip
+	// 已发货
+	StatReturnShipped
+	// 已收货,等待系统确认
+	StatAwaitingConfirm
+	// 售后单已完成
+	StatCompleted
+	// 售后单已取消
+	StatCancelled
+)
+
+const (
+	// 退款申请(部分退款)
+	TypeRefund = 1 + iota
+	// 退货
+	TypeReturn
+	// 换货
+	TypeExchange
+	// 服务/维修
+	TypeService
+)
+
+var (
+	// 不需要平台确认的状态
+	IgnoreConfirmStats = []int{
+		TypeExchange,
+		TypeService,
+	}
+	ErrAfterSalesOrderCompleted *domain.DomainError = domain.NewDomainError(
+		"err_after_sales_order_completed", "售后单已完成,无法进行操作!")
+
+	ErrUnusualStat *domain.DomainError = domain.NewDomainError(
+		"err_after_sales_order_unusual_stat", "不合法的售后单状态")
+
+	ErrNoSuchOrderItem *domain.DomainError = domain.NewDomainError(
+		"err_after_sales_order_no_such_order_item", "订单中不包括该商品")
+
+	ErrItemOutOfQuantity *domain.DomainError = domain.NewDomainError(
+		"err_after_sales_order_out_of_quantity", "商品超出最大数量")
+)
+
 type (
 
 	// 售后单
 	IAfterSalesOrder interface {
 		// 获取领域编号
 		GetDomainId() int
+
+		// 获取订单
+		GetOrder() order.ISubOrder
+
+		// 设置要退回货物信息
+		SetItem(itemId int, quantity int) error
 
 		// 提交售后申请
 		Submit() error
@@ -27,20 +87,17 @@ type (
 		// 同意售后服务
 		Agree() error
 
-		// 系统确认
-		Confirm() error
-
-		// 申请调解
-		RequestIntercede() error
-
 		// 退回商品
 		ReturnShip(spName string, spOrder string, image string) error
 
-		// 已收货
+		// 收货, 在商品已退回或尚未发货情况下(线下退货),可以执行此操作
 		ReturnReceive() error
 
-		// 设置要退回货物信息
-		SetItem(itemId int, quantity int) error
+		// 系统确认
+		Confirm() error
+
+		// 申请调解,只有在商户拒绝后才能申请
+		RequestIntercede() error
 	}
 
 	IAfterSalesRep interface {
@@ -48,11 +105,10 @@ type (
 		CreateRefundOrder(v *RefundOrder) IRefundOrder
 
 		// 获取退款单
-		GetRefundOrder(id int)IRefundOrder
+		GetRefundOrder(id int) IRefundOrder
 
 		// 获取订单的退款单
-		GetRefundOrders(orderId int)[]IRefundOrder
-
+		GetRefundOrders(orderId int) []IRefundOrder
 	}
 
 	// 售后单
@@ -76,11 +132,11 @@ type (
 		// 联系电话
 		PersonPhone string `db:"person_phone"`
 		// 退货的快递服务商编号
-		RefundSpName string `db:"rsp_name"`
+		ReturnSpName string `db:"rsp_name"`
 		// 退货的快递单号
-		RefundSpOrder string `db:"rsp_order"`
+		ReturnSpOrder string `db:"rsp_order"`
 		// 退货凭证
-		RefundSpImage string `db:"rsp_image"`
+		ReturnSpImage string `db:"rsp_image"`
 		// 备注(系统)
 		Remark string `db:"remark"`
 		// 运营商备注
@@ -88,8 +144,8 @@ type (
 		// 售后单状态
 		State int `db:"state"`
 		// 提交时间
-		CreateTime int `db:"create_time"`
+		CreateTime int64 `db:"create_time"`
 		// 更新时间
-		UpdateTime int `db:"update_time"`
+		UpdateTime int64 `db:"update_time"`
 	}
 )
