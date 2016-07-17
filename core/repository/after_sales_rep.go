3a00964167449be5b45d8617dd21fe7dc7a586a2
jarryliu <jarrysix@gmail.com>
2016-07-17 17:29:03 +0800

    after sales order

--- core/repository/after_sales_rep.go
+++ core/repository/after_sales_rep.go
@@ -10,8 +10,8 @@ package repository
 
 import (
 	"github.com/jsix/gof/db"
-	"go2o/core/domain/interface/after-sales"
 	asImpl "go2o/core/domain/after-sales"
+	"go2o/core/domain/interface/after-sales"
 	"go2o/core/domain/interface/order"
 )
 
@@ -22,36 +22,35 @@ type afterSalesRep struct {
 	_orderRep order.IOrderRep
 }
 
-func NewAfterSalesRep(conn db.Connector,orderRep order.IOrderRep) afterSales.IAfterSalesRep {
+func NewAfterSalesRep(conn db.Connector, orderRep order.IOrderRep) afterSales.IAfterSalesRep {
 	return &afterSalesRep{
 		Connector: conn,
-		_orderRep:orderRep,
+		_orderRep: orderRep,
 	}
 
 }
 
 // 创建退款单
 func (a *afterSalesRep) CreateRefundOrder(v *afterSales.RefundOrder) afterSales.IRefundOrder {
-	return asImpl.NewRefundOrder(v,a,a._orderRep)
+	return asImpl.NewRefundOrder(v, a, a._orderRep)
 }
 
-
 // 获取退款单
-func (a *afterSalesRep)GetRefundOrder(id int)afterSales.IRefundOrder{
+func (a *afterSalesRep) GetRefundOrder(id int) afterSales.IRefundOrder {
 	e := &afterSales.RefundOrder{}
-	if a.GetOrm().Get(id,e)== nil{
+	if a.GetOrm().Get(id, e) == nil {
 		return a.CreateRefundOrder(e)
 	}
 	return nil
 }
 
 // 获取订单的退款单
-func (a *afterSalesRep) GetRefundOrders(orderId int)[]afterSales.IRefundOrder{
+func (a *afterSalesRep) GetRefundOrders(orderId int) []afterSales.IRefundOrder {
 	list := []*afterSales.RefundOrder{}
 	orders := []afterSales.IRefundOrder{}
-	if a.GetOrm().Select(&list,"order_id=?",orderId) == nil{
-		for _,v := range list{
-			orders = append(orders,a.CreateRefundOrder(v))
+	if a.GetOrm().Select(&list, "order_id=?", orderId) == nil {
+		for _, v := range list {
+			orders = append(orders, a.CreateRefundOrder(v))
 		}
 	}
 	return orders
