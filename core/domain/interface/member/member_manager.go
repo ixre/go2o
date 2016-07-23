0da7db7c38adf6808c2d4114a3f51cfe5fd1171f
jarryliu <jarrysix@gmail.com>
2016-07-23 12:31:31 +0800

    member level

--- core/domain/interface/member/member_manager.go
+++ core/domain/interface/member/member_manager.go
@@ -9,68 +9,71 @@
 package member
 
 const (
-    RegisterModeClosed = 0 // 关闭注册
-    RegisterModeNormal = 1 // 正常注册
-    RegisterModeMustInvitation = 2 // 必须邀请注册
-    RegisterModeMustRedirect = 3 // 必须直接注册
+	RegisterModeClosed         = 0 // 关闭注册
+	RegisterModeNormal         = 1 // 正常注册
+	RegisterModeMustInvitation = 2 // 必须邀请注册
+	RegisterModeMustRedirect   = 3 // 必须直接注册
 )
 
 type (
 
-    // 会员服务
-    IMemberManager interface {
-        // 等级服务
-        LevelManager() ILevelManager
+	// 会员服务
+	IMemberManager interface {
+		// 等级服务
+		LevelManager() ILevelManager
 
-        // 检测注册权限
-        RegisterPerm(invitation bool) error
+		// 检测注册权限
+		RegisterPerm(invitation bool) error
 
-        // 检查手机绑定,同时检查手机格式
-        CheckPhoneBind(phone string, memberId int) error
+		// 检查手机绑定,同时检查手机格式
+		CheckPhoneBind(phone string, memberId int) error
 
-        // 检查注册信息是否正确
-        CheckPostedRegisterInfo(v *Member, pro *Profile,
-        invitationCode string) (invitationId int, err error)
+		// 检查注册信息是否正确
+		CheckPostedRegisterInfo(v *Member, pro *Profile,
+			invitationCode string) (invitationId int, err error)
 
-        // IDocManager()IDocManager
-    }
+		// IDocManager()IDocManager
+	}
 
-    //会员等级
-    Level struct {
-        //编号
-        Id            int `db:"id" auto:"yes" pk:"yes"`
-        //等级名称
-        Name          string `db:"name"`
-        //需要经验值
-        RequireExp    int `db:"require_exp"`
-        // 可编程等级签名,可根据此签名来进行编程
-        ProgramSignal string `db:"program_signal"`
-        //是否启用
-        Enabled       int `db:"enabled"`
-        //是否为正式会员/非正式
-        IsOfficial    int `db:"is_official"`
-    }
+	//会员等级
+	Level struct {
+		//编号
+		Id int `db:"id" auto:"yes" pk:"yes"`
+		//等级名称
+		Name string `db:"name"`
+		//需要经验值
+		RequireExp int `db:"require_exp"`
+		// 可编程等级签名,可根据此签名来进行编程
+		ProgramSignal string `db:"program_signal"`
+		//是否启用
+		Enabled int `db:"enabled"`
+		//是否为正式会员/非正式
+		IsOfficial int `db:"is_official"`
+	}
 
-    ILevelManager interface {
-        // 获取等级设置
-        GetLevelSet() []*Level
+	ILevelManager interface {
+		// 获取等级设置
+		GetLevelSet() []*Level
 
-        // 获取等级
-        GetLevelById(id int) *Level
+		// 获取最高已启用的等级
+		GetHighestLevel() *Level
 
-        // 根据可编程字符获取会员等级
-        GetLevelByProgramSign(sign string) *Level
+		// 获取等级
+		GetLevelById(id int) *Level
 
-        // 获取下一个等级
-        GetNextLevelById(int int) *Level
+		// 根据可编程字符获取会员等级
+		GetLevelByProgramSign(sign string) *Level
 
-        // 删除等级
-        DeleteLevel(id int) error
+		// 获取下一个等级
+		GetNextLevelById(int int) *Level
 
-        // 保存等级
-        SaveLevel(*Level) (int, error)
+		// 删除等级
+		DeleteLevel(id int) error
 
-        // 根据经验值获取等级值
-        GetLevelIdByExp(exp int) int
-    }
+		// 保存等级
+		SaveLevel(*Level) (int, error)
+
+		// 根据经验值获取等级值
+		GetLevelIdByExp(exp int) int
+	}
 )
