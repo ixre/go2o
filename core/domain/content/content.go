e9ef250ee64618acd65ec7a0d32450b4b2d00d1d
jarryliu <jarrysix@gmail.com>
2016-09-04 14:48:12 +0800

    article
    Former-commit-id: fe93dd05717ac916c47b0dfb218098f831b2f8b9

--- core/domain/content/content.go
+++ core/domain/content/content.go
@@ -26,42 +26,42 @@ func NewContent(userId int, rep content.IContentRep) content.IContent {
 }
 
 // 获取聚合根编号
-func (this *Content) GetAggregateRootId() int {
-	return this._userId
+func (c *Content) GetAggregateRootId() int {
+	return c._userId
 }
 
 // 文章服务
-func (this *Content) ArticleManager() content.IArticleManager {
-	if this._articleManager == nil {
-		this._articleManager = newArticleManagerImpl(this._userId, this._contentRep)
+func (c *Content) ArticleManager() content.IArticleManager {
+	if c._articleManager == nil {
+		c._articleManager = newArticleManagerImpl(c._userId, c._contentRep)
 	}
-	return this._articleManager
+	return c._articleManager
 }
 
 // 创建页面
-func (this *Content) CreatePage(v *content.Page) content.IPage {
-	return newPage(this.GetAggregateRootId(), this._contentRep, v)
+func (c *Content) CreatePage(v *content.Page) content.IPage {
+	return newPage(c.GetAggregateRootId(), c._contentRep, v)
 }
 
 // 获取页面
-func (this *Content) GetPage(id int) content.IPage {
-	v := this._contentRep.GetPageById(this.GetAggregateRootId(), id)
+func (c *Content) GetPage(id int) content.IPage {
+	v := c._contentRep.GetPageById(c.GetAggregateRootId(), id)
 	if v != nil {
-		return this.CreatePage(v)
+		return c.CreatePage(v)
 	}
 	return nil
 }
 
 // 根据字符串标识获取页面
-func (this *Content) GetPageByStringIndent(indent string) content.IPage {
-	v := this._contentRep.GetPageByStringIndent(this.GetAggregateRootId(), indent)
+func (c *Content) GetPageByStringIndent(indent string) content.IPage {
+	v := c._contentRep.GetPageByStringIndent(c.GetAggregateRootId(), indent)
 	if v != nil {
-		return this.CreatePage(v)
+		return c.CreatePage(v)
 	}
 	return nil
 }
 
 // 删除页面
-func (this *Content) DeletePage(id int) error {
-	return this._contentRep.DeletePage(this.GetAggregateRootId(), id)
+func (c *Content) DeletePage(id int) error {
+	return c._contentRep.DeletePage(c.GetAggregateRootId(), id)
 }
