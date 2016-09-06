ea9a3babf49237ab32186b882f7966fa48a0359a
jarryliu <jarrysix@gmail.com>
2016-09-06 18:55:31 +0800

    value rep
    Former-commit-id: 393b6218c0ae3a7232728658df4d4edca58f29d8

--- core/domain/interface/valueobject/value_rep.go
+++ core/domain/interface/valueobject/value_rep.go
@@ -11,258 +11,266 @@ package valueobject
 import "go2o/core/domain/interface/enum"
 
 var (
-    TradeCsnTypeByOrder = 1 //按订单笔数收取手续费
-    TradeCsnTypeByFee = 2 //按交易金额收取手续费
+	TradeCsnTypeByOrder = 1 //按订单笔数收取手续费
+	TradeCsnTypeByFee   = 2 //按交易金额收取手续费
 )
 
 var (
-    SmsAli = 1 //阿里大鱼
-    SmsNetEasy = 2 //网易
-    Sms253Com = 3 //创蓝
-    SmsIndex = []int{
-        SmsAli,
-        SmsNetEasy,
-        Sms253Com,
-    }
-    SmsTextMap = map[int]string{
-        SmsAli:     "阿里大鱼",
-        SmsNetEasy: "网易",
-        Sms253Com:  "创蓝",
-    }
+	SmsAli     = 1 //阿里大鱼
+	SmsNetEasy = 2 //网易
+	Sms253Com  = 3 //创蓝
+	SmsIndex   = []int{
+		SmsAli,
+		SmsNetEasy,
+		Sms253Com,
+	}
+	SmsTextMap = map[int]string{
+		SmsAli:     "阿里大鱼",
+		SmsNetEasy: "网易",
+		Sms253Com:  "创蓝",
+	}
 )
 
 type (
-    // 平台设置
-    PlatformConf struct {
-        // 平台名称
-        Name                    string
-        // 标志
-        Logo                    string
-        // 允许商户创建商品分类
-        MchGoodsCategory        bool
-        // 允许商户创建页面分类
-        MchPageCategory         bool
-        // 是否启用会员经验值
-        EnabledMemberExperience bool
-    }
+	// 平台设置
+	PlatformConf struct {
+		// 平台名称
+		Name string
+		// 标志
+		Logo string
+		// 允许商户创建商品分类
+		MchGoodsCategory bool
+		// 允许商户创建页面分类
+		MchPageCategory bool
+		// 是否启用会员经验值
+		EnabledMemberExperience bool
+	}
 
-    Registry struct {
-        // 商户提现是否免费
-        MerchantTakeOutCashFree            bool
+	Registry struct {
+		// 商户提现是否免费
+		MerchantTakeOutCashFree bool
 
-        // 收货时的提示信息
-        OrderReceiveAlertMessage           string
-        // 会员资料不完善提醒信息
-        MemberProfileNotCompletedMessage   string
-        // 会员转账提示信息
-        MemberTransferAccountsAlertMessage string
-        // 会员实名提醒信息
-        MemberNotTrustedMessage            string
-        // 注册后赠送积分数量
-        PresentIntegralNumOfRegister       int
-        // 会员邀请关系级数
-        MemberReferLayer                   int
-        // 会员即时通讯是否必须
-        MemberImRequired                   bool
-        // 商品默认图片
-        GoodsDefaultImage                  string
-        // 商品最低利润率,既(销售价-供货价)/销售价的比例
-        GoodsMinProfitRate                 float32
-        // 会员中心首页模板文件名称
-        PageUCenterIndexTpl                string
-        // 平台首页模板文件
-        PageIndexTpl string
-        // 其他扩展数据
-        Extend                             map[string]string
-    }
+		// 收货时的提示信息
+		OrderReceiveAlertMessage string
+		// 会员资料不完善提醒信息
+		MemberProfileNotCompletedMessage string
+		// 会员转账提示信息
+		MemberTransferAccountsAlertMessage string
+		// 会员实名提醒信息
+		MemberNotTrustedMessage string
+		// 注册后赠送积分数量
+		PresentIntegralNumOfRegister int
+		// 会员邀请关系级数
+		MemberReferLayer int
+		// 会员即时通讯是否必须
+		MemberImRequired bool
+		// 商品默认图片
+		GoodsDefaultImage string
+		// 商品最低利润率,既(销售价-供货价)/销售价的比例
+		GoodsMinProfitRate float32
+		// 平台首页模板文件
+		PagePlatformPath string
+		// 平台移动首页模板文件
+		PageMobilePlatformPath string
+		// 商铺首页模板文件
+		PageShopPath string
+		// 商铺移动首页模板文件
+		PageMobileShopPath string
+		// 会员中心首页模板文件名称
+		PageUCenterPath string
+		// 会员中心移动首页模板文件名称
+		PageMobileUCenterPath string
+		// 其他扩展数据
+		Extend map[string]string
+	}
 
-    // 微信API设置
-    //todo: ??? 应在应用层
-    WxApiConfig struct {
-        /**===== 微信公众平台设置 =====**/
+	// 微信API设置
+	//todo: ??? 应在应用层
+	WxApiConfig struct {
+		/**===== 微信公众平台设置 =====**/
 
-        //APP ID
-        AppId               string
-        //APP 密钥
-        AppSecret           string
-        //通信密钥
-        MpToken             string
-        //通信AES KEY
-        MpAesKey            string
-        //原始ID
-        OriId               string
+		//APP ID
+		AppId string
+		//APP 密钥
+		AppSecret string
+		//通信密钥
+		MpToken string
+		//通信AES KEY
+		MpAesKey string
+		//原始ID
+		OriId string
 
-        /**===== 用于微信支付 =====**/
+		/**===== 用于微信支付 =====**/
 
-        //商户编号
-        MchId               string
-        //商户接口密钥
-        MchApiKey           string
-        //微信支付的证书路径(上传)
-        MchCertPath         string
-        //微信支付的证书公钥路径(上传)
-        MchCertKeyPath      string
-        //是否启用红包功能
-        RedPackEnabled      bool
-        //红包金额限制
-        RedPackAmountLimit  float32
-        //红包每日数量限制
-        RedPackDayTimeLimit int
+		//商户编号
+		MchId string
+		//商户接口密钥
+		MchApiKey string
+		//微信支付的证书路径(上传)
+		MchCertPath string
+		//微信支付的证书公钥路径(上传)
+		MchCertKeyPath string
+		//是否启用红包功能
+		RedPackEnabled bool
+		//红包金额限制
+		RedPackAmountLimit float32
+		//红包每日数量限制
+		RedPackDayTimeLimit int
 
-        //MchPayNotifyPath string //微信支付异步通知的路径
-    }
+		//MchPayNotifyPath string //微信支付异步通知的路径
+	}
 
-    // 注册权限设置
-    RegisterPerm struct {
-        // 注册模式,等于member.RegisterMode
-        RegisterMode        int
-        // 是否允许匿名注册
-        AnonymousRegistered bool
-        // 手机号码作为用户名
-        PhoneAsUser         bool
-        // 是否需要填写手机
-        NeedPhone           bool
-        // 必须绑定手机
-        MustBinPhone        bool
-        // 是否需要填写即时通讯
-        NeedIm              bool
-        // 注册提示
-        Notice              string
-        // 用户条款内容
-        Licence             string
-        // 注册回调页
-        CallBackUrl         string
-    }
+	// 注册权限设置
+	RegisterPerm struct {
+		// 注册模式,等于member.RegisterMode
+		RegisterMode int
+		// 是否允许匿名注册
+		AnonymousRegistered bool
+		// 手机号码作为用户名
+		PhoneAsUser bool
+		// 是否需要填写手机
+		NeedPhone bool
+		// 必须绑定手机
+		MustBinPhone bool
+		// 是否需要填写即时通讯
+		NeedIm bool
+		// 注册提示
+		Notice string
+		// 用户条款内容
+		Licence string
+		// 注册回调页
+		CallBackUrl string
+	}
 
-    // 全局数值设置
-    GlobNumberConf struct {
-        // 兑换1元所需要的积分
-        IntegralExchangeRate      float32
-        // 抵扣1元所需要的积分
-        IntegralDiscountRate      float32
-        // 消费1元产生的经验
-        ExperienceRateByOrder     float32
-        // 消费1元产生的积分
-        IntegralRateByConsumption float32
-        // 每单额外赠送
-        IntegralBackExtra         int
-        // 提现手续费费率
-        ApplyCsn                  float32
-        // 转账手续费费率
-        TransCsn                  float32
-        // 活动账户转为赠送可提现奖金手续费费率
-        FlowConvertCsn            float32
-        // 赠送账户转换手续费费率
-        PresentConvertCsn         float32
-        // 交易手续费类型
-        TradeCsnType              int
-        // 按交易笔数收取手续费的金额
-        TradeCsnFeeByOrder        float32
-        // 按交易金额收取手续费的百分百
-        TradeCsnPercentByFee      float32
-        // 最低提现金额
-        MinApplyAmount            float32
-        // 单笔最高提现金额
-        MaxApplyAmount            float32
-    }
+	// 全局数值设置
+	GlobNumberConf struct {
+		// 兑换1元所需要的积分
+		IntegralExchangeRate float32
+		// 抵扣1元所需要的积分
+		IntegralDiscountRate float32
+		// 消费1元产生的经验
+		ExperienceRateByOrder float32
+		// 消费1元产生的积分
+		IntegralRateByConsumption float32
+		// 每单额外赠送
+		IntegralBackExtra int
+		// 提现手续费费率
+		ApplyCsn float32
+		// 转账手续费费率
+		TransCsn float32
+		// 活动账户转为赠送可提现奖金手续费费率
+		FlowConvertCsn float32
+		// 赠送账户转换手续费费率
+		PresentConvertCsn float32
+		// 交易手续费类型
+		TradeCsnType int
+		// 按交易笔数收取手续费的金额
+		TradeCsnFeeByOrder float32
+		// 按交易金额收取手续费的百分百
+		TradeCsnPercentByFee float32
+		// 最低提现金额
+		MinApplyAmount float32
+		// 单笔最高提现金额
+		MaxApplyAmount float32
+	}
 
-    // 全局商户销售设置
-    GlobMchSaleConf struct {
-        // 商户订单结算模式
-        MchOrderSettleMode      enum.MchSettleMode
-        // 商户订单结算比例
-        MchOrderSettleRate      float32
-        // 是否启用分销模式
-        FxSalesEnabled          bool
-        // 返现比例,0则不返现
-        CashBackPercent         float32
-        // 一级比例
-        CashBackTg1Percent      float32
-        // 二级比例
-        CashBackTg2Percent      float32
-        // 会员比例
-        CashBackMemberPercent   float32
+	// 全局商户销售设置
+	GlobMchSaleConf struct {
+		// 商户订单结算模式
+		MchOrderSettleMode enum.MchSettleMode
+		// 商户订单结算比例
+		MchOrderSettleRate float32
+		// 是否启用分销模式
+		FxSalesEnabled bool
+		// 返现比例,0则不返现
+		CashBackPercent float32
+		// 一级比例
+		CashBackTg1Percent float32
+		// 二级比例
+		CashBackTg2Percent float32
+		// 会员比例
+		CashBackMemberPercent float32
 
-        // 自动设置订单
-        AutoSetupOrder          int
-        // 订单超时分钟数
-        OrderTimeOutMinute      int
-        // 订单自动确认时间
-        OrderConfirmAfterMinute int
-        // 订单超时自动收货
-        OrderTimeOutReceiveHour int
-    }
+		// 自动设置订单
+		AutoSetupOrder int
+		// 订单超时分钟数
+		OrderTimeOutMinute int
+		// 订单自动确认时间
+		OrderConfirmAfterMinute int
+		// 订单超时自动收货
+		OrderTimeOutReceiveHour int
+	}
 
-    IValueRep interface {
-        // 获取微信接口配置
-        GetWxApiConfig() WxApiConfig
+	IValueRep interface {
+		// 获取微信接口配置
+		GetWxApiConfig() WxApiConfig
 
-        // 保存微信接口配置
-        SaveWxApiConfig(v *WxApiConfig) error
+		// 保存微信接口配置
+		SaveWxApiConfig(v *WxApiConfig) error
 
-        // 获取注册权限
-        GetRegisterPerm() RegisterPerm
+		// 获取注册权限
+		GetRegisterPerm() RegisterPerm
 
-        // 保存注册权限
-        SaveRegisterPerm(v *RegisterPerm) error
+		// 保存注册权限
+		SaveRegisterPerm(v *RegisterPerm) error
 
-        // 获取全局系统数值设置
-        GetGlobNumberConf() GlobNumberConf
+		// 获取全局系统数值设置
+		GetGlobNumberConf() GlobNumberConf
 
-        // 保存全局系统数值设置
-        SaveGlobNumberConf(v *GlobNumberConf) error
+		// 保存全局系统数值设置
+		SaveGlobNumberConf(v *GlobNumberConf) error
 
-        // 获取平台设置
-        GetPlatformConf() PlatformConf
+		// 获取平台设置
+		GetPlatformConf() PlatformConf
 
-        // 保存平台设置
-        SavePlatformConf(v *PlatformConf) error
+		// 保存平台设置
+		SavePlatformConf(v *PlatformConf) error
 
-        // 获取数据存储
-        GetRegistry() Registry
+		// 获取数据存储
+		GetRegistry() Registry
 
-        // 保存数据存储
-        SaveRegistry(v *Registry) error
+		// 保存数据存储
+		SaveRegistry(v *Registry) error
 
-        // 获取全局商户销售设置
-        GetGlobMchSaleConf() GlobMchSaleConf
+		// 获取全局商户销售设置
+		GetGlobMchSaleConf() GlobMchSaleConf
 
-        // 保存全局商户销售设置
-        SaveGlobMchSaleConf(v *GlobMchSaleConf) error
+		// 保存全局商户销售设置
+		SaveGlobMchSaleConf(v *GlobMchSaleConf) error
 
-        // 获取短信设置
-        GetSmsApiSet() SmsApiSet
+		// 获取短信设置
+		GetSmsApiSet() SmsApiSet
 
-        // 保存短信API
-        SaveSmsApiPerm(provider int, s *SmsApiPerm) error
+		// 保存短信API
+		SaveSmsApiPerm(provider int, s *SmsApiPerm) error
 
-        // 获取默认的短信API,返回API提供商编号及API信息
-        GetDefaultSmsApiPerm() (int, *SmsApiPerm)
+		// 获取默认的短信API,返回API提供商编号及API信息
+		GetDefaultSmsApiPerm() (int, *SmsApiPerm)
 
-        // 获取下级区域
-        GetChildAreas(id int) []*Area
+		// 获取下级区域
+		GetChildAreas(id int) []*Area
 
-        // 获取地区名称
-        GetAreaNames(id []int) []string
+		// 获取地区名称
+		GetAreaNames(id []int) []string
 
-        // 获取省市区字符串
-        GetAreaString(province, city, district int) string
-    }
+		// 获取省市区字符串
+		GetAreaString(province, city, district int) string
+	}
 
-    // 短信接口
-    SmsApiPerm struct {
-        ApiKey    string //接口编号
-        ApiSecret string //接口密钥
-        Default   bool   //是否默认的接口使用
-    }
-    // 短信接口设置
-    SmsApiSet map[int]*SmsApiPerm
+	// 短信接口
+	SmsApiPerm struct {
+		ApiKey    string //接口编号
+		ApiSecret string //接口密钥
+		Default   bool   //是否默认的接口使用
+	}
+	// 短信接口设置
+	SmsApiSet map[int]*SmsApiPerm
 
-    //http://www.stats.gov.cn/tjsj/tjbz/xzqhdm/
-    // 区域,中国行政区划
-    Area struct {
-        Code   int    `db:"code" json:"code"`
-        Parent int    `db:"parent" json:"parent"`
-        Name   string `db:"name" json:"name"`
-    }
+	//http://www.stats.gov.cn/tjsj/tjbz/xzqhdm/
+	// 区域,中国行政区划
+	Area struct {
+		Code   int    `db:"code" json:"code"`
+		Parent int    `db:"parent" json:"parent"`
+		Name   string `db:"name" json:"name"`
+	}
 )
--- core/repository/value_default.go
+++ core/repository/value_default.go
@@ -9,121 +9,117 @@
 package repository
 
 import (
-    "go2o/core/domain/interface/enum"
-    "go2o/core/domain/interface/member"
-    "go2o/core/domain/interface/valueobject"
+	"go2o/core/domain/interface/enum"
+	"go2o/core/domain/interface/member"
+	"go2o/core/domain/interface/valueobject"
 )
 
 var (
-    DefaultRegistry = valueobject.Registry{
-        // 商户提现是否免费
-        MerchantTakeOutCashFree: true,
-        // 收货提示信息
-        OrderReceiveAlertMessage: "确认收货后,款项将转给商户。请在收货前确保已经商品没有损坏和缺少!",
+	DefaultRegistry = valueobject.Registry{
+		// 商户提现是否免费
+		MerchantTakeOutCashFree: true,
+		// 收货提示信息
+		OrderReceiveAlertMessage: "确认收货后,款项将转给商户。请在收货前确保已经商品没有损坏和缺少!",
 
-        MemberProfileNotCompletedMessage: "您的个人资料未完善,是否立即完善?",
-        // 会员转账提示信息
-        MemberTransferAccountsAlertMessage: "平台仅提供转账功能，请尽量当面交易以保证安全！",
-        // 会员未实名认证提示信息
-        MemberNotTrustedMessage: "您尚未实名认证!",
-        // 注册后赠送积分数量
-        PresentIntegralNumOfRegister: 0,
-        MemberReferLayer:             3,
-        // 会员即时通讯是否必须
-        MemberImRequired: false,
-        // 商品默认图片
-        GoodsDefaultImage: "res/nopic.gif",
-        // 商品最低利润率,既(销售价-供货价)/销售价的比例
-        GoodsMinProfitRate: 0,
-        // 平台首页模板文件
-        PageIndexTpl:"index.html",
-        // 会员中心首页模板文件名称
-        PageUCenterIndexTpl:              "index.html",
-        Extend:             map[string]string{},
-    }
+		MemberProfileNotCompletedMessage: "您的个人资料未完善,是否立即完善?",
+		// 会员转账提示信息
+		MemberTransferAccountsAlertMessage: "平台仅提供转账功能，请尽量当面交易以保证安全！",
+		// 会员未实名认证提示信息
+		MemberNotTrustedMessage: "您尚未实名认证!",
+		// 注册后赠送积分数量
+		PresentIntegralNumOfRegister: 0,
+		MemberReferLayer:             3,
+		// 会员即时通讯是否必须
+		MemberImRequired: false,
+		// 商品默认图片
+		GoodsDefaultImage: "res/nopic.gif",
+		// 商品最低利润率,既(销售价-供货价)/销售价的比例
+		GoodsMinProfitRate: 0,
+		Extend:             map[string]string{},
+	}
 
-    // 默认平台设置
-    //todo: 默认值
-    defaultPlatformConf = valueobject.PlatformConf{
-        Name:                    "GO2O",
-        Logo:                    "https://raw.githubusercontent.com/jsix/go2o/master/docs/mark.gif",
-        MchGoodsCategory:        false,
-        MchPageCategory:         false,
-        EnabledMemberExperience: !true,
-    }
+	// 默认平台设置
+	//todo: 默认值
+	defaultPlatformConf = valueobject.PlatformConf{
+		Name:                    "GO2O",
+		Logo:                    "https://raw.githubusercontent.com/jsix/go2o/master/docs/mark.gif",
+		MchGoodsCategory:        false,
+		MchPageCategory:         false,
+		EnabledMemberExperience: !true,
+	}
 
-    // 默认注册权限设置
-    defaultRegisterPerm = valueobject.RegisterPerm{
-        RegisterMode:        member.RegisterModeNormal,
-        NeedPhone:           false,
-        MustBinPhone:        false,
-        NeedIm:              false,
-        AnonymousRegistered: true,
-        PhoneAsUser:         false,
-        CallBackUrl:         "/auth?uc=1", //默认进入会员中心
-    }
+	// 默认注册权限设置
+	defaultRegisterPerm = valueobject.RegisterPerm{
+		RegisterMode:        member.RegisterModeNormal,
+		NeedPhone:           false,
+		MustBinPhone:        false,
+		NeedIm:              false,
+		AnonymousRegistered: true,
+		PhoneAsUser:         false,
+		CallBackUrl:         "/auth?uc=1", //默认进入会员中心
+	}
 
-    // 默认全局销售设置
-    DefaultGlobNumberConf = valueobject.GlobNumberConf{
-        // 兑换1元所需要的积分
-        IntegralExchangeRate: 100,
-        // 抵扣1元所需要的积分
-        IntegralDiscountRate: 100,
-        // 消费1元产生的经验
-        ExperienceRateByOrder: 1,
-        // 消费1元产生的积分
-        IntegralRateByConsumption: 1,
-        // 每单额外赠送
-        IntegralBackExtra: 0,
-        // 提现手续费费率
-        ApplyCsn: 0.01,
-        // 转账手续费费率
-        TransCsn: 0.01,
-        // 活动账户转为赠送可提现奖金手续费费率
-        FlowConvertCsn: 0.05,
-        // 赠送账户转换手续费费率
-        PresentConvertCsn: 0.05,
-        // 交易手续费类型
-        TradeCsnType: valueobject.TradeCsnTypeByFee,
-        // 按交易笔数收取手续费的金额
-        TradeCsnFeeByOrder: 1, // 每笔订单最低收取1元
-        // 按交易金额收取手续费的百分百
-        TradeCsnPercentByFee: 0.01, // 1%收取
-        MinApplyAmount:       0.01,
-        // 单笔最高提现金额
-        MaxApplyAmount: 1000,
-    }
+	// 默认全局销售设置
+	DefaultGlobNumberConf = valueobject.GlobNumberConf{
+		// 兑换1元所需要的积分
+		IntegralExchangeRate: 100,
+		// 抵扣1元所需要的积分
+		IntegralDiscountRate: 100,
+		// 消费1元产生的经验
+		ExperienceRateByOrder: 1,
+		// 消费1元产生的积分
+		IntegralRateByConsumption: 1,
+		// 每单额外赠送
+		IntegralBackExtra: 0,
+		// 提现手续费费率
+		ApplyCsn: 0.01,
+		// 转账手续费费率
+		TransCsn: 0.01,
+		// 活动账户转为赠送可提现奖金手续费费率
+		FlowConvertCsn: 0.05,
+		// 赠送账户转换手续费费率
+		PresentConvertCsn: 0.05,
+		// 交易手续费类型
+		TradeCsnType: valueobject.TradeCsnTypeByFee,
+		// 按交易笔数收取手续费的金额
+		TradeCsnFeeByOrder: 1, // 每笔订单最低收取1元
+		// 按交易金额收取手续费的百分百
+		TradeCsnPercentByFee: 0.01, // 1%收取
+		MinApplyAmount:       0.01,
+		// 单笔最高提现金额
+		MaxApplyAmount: 1000,
+	}
 
-    DefaultGlobMchSaleConf = valueobject.GlobMchSaleConf{
-        // 商户订单结算模式
-        MchOrderSettleMode: enum.MchModeSettleByRate,
-        // 商户订单结算比例
-        MchOrderSettleRate: 1,
-        // 是否启用分销模式
-        FxSalesEnabled: false,
-        // 返现比例,0则不返现
-        CashBackPercent: 0.1,
-        // 一级比例
-        CashBackTg1Percent: 0.5,
-        // 二级比例
-        CashBackTg2Percent: 0.3,
-        // 会员比例
-        CashBackMemberPercent: 0.2,
+	DefaultGlobMchSaleConf = valueobject.GlobMchSaleConf{
+		// 商户订单结算模式
+		MchOrderSettleMode: enum.MchModeSettleByRate,
+		// 商户订单结算比例
+		MchOrderSettleRate: 1,
+		// 是否启用分销模式
+		FxSalesEnabled: false,
+		// 返现比例,0则不返现
+		CashBackPercent: 0.1,
+		// 一级比例
+		CashBackTg1Percent: 0.5,
+		// 二级比例
+		CashBackTg2Percent: 0.3,
+		// 会员比例
+		CashBackMemberPercent: 0.2,
 
-        // 自动设置订单
-        AutoSetupOrder: 1,
-        // 订单超时分钟数
-        OrderTimeOutMinute: 720, // 12小时
-        // 订单自动确认时间
-        OrderConfirmAfterMinute: 10,
-        // 订单超时自动收货
-        OrderTimeOutReceiveHour: 168, //7天
-    }
+		// 自动设置订单
+		AutoSetupOrder: 1,
+		// 订单超时分钟数
+		OrderTimeOutMinute: 720, // 12小时
+		// 订单自动确认时间
+		OrderConfirmAfterMinute: 10,
+		// 订单超时自动收货
+		OrderTimeOutReceiveHour: 168, //7天
+	}
 
-    // 默认短信接口设置
-    defaultSmsConf = map[int]*valueobject.SmsApiPerm{
-        valueobject.SmsAli:     &valueobject.SmsApiPerm{Default: true},
-        valueobject.SmsNetEasy: &valueobject.SmsApiPerm{},
-        valueobject.Sms253Com:  &valueobject.SmsApiPerm{},
-    }
+	// 默认短信接口设置
+	defaultSmsConf = map[int]*valueobject.SmsApiPerm{
+		valueobject.SmsAli:     &valueobject.SmsApiPerm{Default: true},
+		valueobject.SmsNetEasy: &valueobject.SmsApiPerm{},
+		valueobject.Sms253Com:  &valueobject.SmsApiPerm{},
+	}
 )
