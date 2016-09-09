54153feb7708d6d0e81f43efd8a2009142ea1cd3
jarryliu <jarrysix@gmail.com>
2016-09-09 21:27:36 +0800

    hapi
    Former-commit-id: d842a16b67b714bc11e8a2025f0a0a65b650c1cc

--- app/web/hapi/main.go
+++ app/web/hapi/main.go
@@ -12,14 +12,28 @@ import (
     "github.com/jsix/gof"
     "gopkg.in/labstack/echo.v1"
     "net/http"
+    "go2o/x/echox"
 )
 
-type mainC struct{
+type mainC struct {
     gof.App
 }
 
-func (m *mainC) Info(ctx *echo.Context)error{
-    return ctx.String(http.StatusOK,`
+func (m *mainC) Info(ctx *echo.Context) error {
+    return ctx.String(http.StatusOK, `
         release : 2016-09-10
     `)
+}
+
+// 测试HAPI
+func (m *mainC) Test(ctx *echox.Context) error {
+    memberId := getMemberId(ctx)
+    if memberId <= 0 {
+        return requestLogin(ctx)
+    }
+    d := gof.Message{
+        Result:true,
+        Data:memberId,
+    }
+    return ctx.JSONP(http.StatusOK, ctx.Query("callback"), d)
 }
--- app/web/hapi/serve.go
+++ app/web/hapi/serve.go
@@ -11,25 +11,51 @@ package hapi
 import (
     "gopkg.in/labstack/echo.v1"
     "github.com/jsix/gof"
+    "go2o/app/web/shared"
+    "go2o/x/echox"
+    "go2o/core/domain/interface/member"
+    "net/http"
 )
 
-func NewServe() *echo.Echo {
-    s := echo.New()
+func NewServe() *echox.Echo {
+    s := echox.New()
     //s.Use(mw.Recover())
-   // s.Use(echox.StopAttack)
+    // s.Use(echox.StopAttack)
     s.Use(beforeHanding)
     registerRoutes(s)
     return s
 }
 
-func registerRoutes(s *echo.Echo){
+func registerRoutes(s *echox.Echo) {
     app := gof.CurrentApp
     mc := &mainC{app}
-    s.Get("/api_info",mc.Info)
+    uc := &shared.UserC{App: app}
+    s.Get("/api_info", mc.Info)
+    s.Getx("/test", mc.Test)
+    s.Agetx("/user/:action", uc)
 }
 
 func beforeHanding(h echo.HandlerFunc) echo.HandlerFunc {
     return func(ctx *echo.Context) error {
         return h(ctx)
     }
+}
+
+//检查会员编号
+func getMemberId(ctx *echox.Context)int{
+    v := ctx.Session.Get("member")
+    if v != nil {
+        m := v.(*member.Member)
+        if m != nil {
+            return m.Id
+        }
+    }
+    return -1
+}
+
+func requestLogin(ctx *echox.Context)error{
+    msg := gof.Message{
+        Message:"not login",
+    }
+    return ctx.JSONP(http.StatusOK,ctx.Query("callback"),msg)
 }
