e5f6e2932b04b72f9347bc056a0c11aa9630ecea
jarryliu <jarrysix@gmail.com>
2016-10-02 11:20:05 +0800

    shared.go
    Former-commit-id: c989e151b9b3cfea427cad089362da0c4684ad1f

--- app/web/shared/shared.go
+++ app/web/shared/shared.go
@@ -9,59 +9,38 @@
 package shared
 
 import (
-    "github.com/jsix/gof/web"
-    "go2o/x/echox"
-    "gopkg.in/labstack/echo.v1"
-    "html/template"
-    "github.com/jsix/gof/web/session"
-    "github.com/jsix/gof/storage"
-    "strconv"
-    "errors"
+	"github.com/jsix/gof/web"
+	"go2o/x/echox"
+	"gopkg.in/labstack/echo.v1"
+	"html/template"
 )
 
 const (
-    AppPlatform = 1 << iota
-    AppMerchant
-    AppPassport
-    AppUCenter
-    AppShop
+	AppPlatform = 1 << iota
+	AppMerchant
+	AppPassport
+	AppUCenter
+	AppShop
 )
 
 var (
-    // 模板监视更改
-    TemplateObserverFlag int = AppPlatform | AppMerchant | AppPassport | AppUCenter | AppShop
+	// 模板监视更改
+	TemplateObserverFlag int = AppPlatform | AppMerchant | AppPassport | AppUCenter | AppShop
 )
 
 // 提示页面
 func RenderMessagePage(c *echox.Context, msg string, btn string, url string) error {
-    d := c.NewData()
-    d.Map = map[string]interface{}{
-        "Message":    template.HTML(msg),
-        "ButtonText": btn,
-        "HasButton":  btn != "",
-        "Url":        url,
-    }
-    return c.RenderOK("message_page.html", d)
+	d := c.NewData()
+	d.Map = map[string]interface{}{
+		"Message":    template.HTML(msg),
+		"ButtonText": btn,
+		"HasButton":  btn != "",
+		"Url":        url,
+	}
+	return c.RenderOK("message_page.html", d)
 }
 
+// 处理HTTP错误
 func HandleHttpError(err error, ctx *echo.Context) {
-    web.HttpError(ctx.Response(), err)
-}
-
-//  存储会员的会话映射
-func SaveMemberSession(sto storage.Interface, s *session.Session, memberId int) {
-    sessionId := s.GetSessionId()
-    sto.SetExpire("go2o:ss:" + strconv.Itoa(memberId),
-        sessionId, s.MaxAge())
-}
-
-// 重设会员的登陆会话
-func CleanMemberSession(sto storage.Interface, memberId int) error {
-    sessionId, err := sto.GetString("go2o:ss:" + strconv.Itoa(memberId))
-    if err == nil {
-        storageKey := session.GetStorageKey(sessionId)
-        sto.Del(storageKey)
-        return nil
-    }
-    return errors.New("会员当前没有登陆")
+	web.HttpError(ctx.Response(), err)
 }
