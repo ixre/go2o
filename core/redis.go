39855185d46118d8b4c5745f3d5b5482f5a2b67b
jarryliu <jarrysix@gmail.com>
2016-10-02 16:48:25 +0800

    redis

--- core/redis.go
+++ core/redis.go
@@ -9,123 +9,123 @@
 package core
 
 import (
-	"errors"
-	"fmt"
-	"github.com/garyburd/redigo/redis"
-	"github.com/jsix/gof"
-	"github.com/jsix/gof/storage"
-	"log"
-	"time"
+    "errors"
+    "fmt"
+    "github.com/garyburd/redigo/redis"
+    "github.com/jsix/gof"
+    "github.com/jsix/gof/storage"
+    "log"
+    "time"
 )
 
 var (
-	hasGet        bool = false
-	globPool      *redis.Pool
-	CleanHookFunc func(gof.App) // 当清理数据时候发生
+    hasGet bool = false
+    globPool      *redis.Pool
+    CleanHookFunc func(gof.App) // 当清理数据时候发生
 )
 
 func CreateRedisPool(c *gof.Config) *redis.Pool {
-	redisHost := c.GetString("redis_host")
-	redisDb := c.GetString("redis_db")
-	redisPort := c.GetInt("redis_port")
-	redisAuth := c.GetString("redis_auth")
+    host := c.GetString("redis_host")
+    db := c.GetString("redis_db")
+    port := c.GetInt("redis_port")
+    auth := c.GetString("redis_auth")
 
-	if redisPort <= 0 {
-		redisPort = 6379
-	}
-	redisMaxIdle := c.GetInt("redis_maxIdle")
-	if redisMaxIdle <= 0 {
-		redisMaxIdle = 10000
-	}
-	redisIdleTimeout := c.GetInt("redis_idleTimeout")
-	if redisIdleTimeout <= 0 {
-		redisIdleTimeout = 20000
-	}
+    if port <= 0 {
+        port = 6379
+    }
+    maxIdle := c.GetInt("redis_maxIdle")
+    if maxIdle <= 0 {
+        maxIdle = 10000
+    }
+    idleTimeout := c.GetInt("redis_idleTimeout")
+    if idleTimeout <= 0 {
+        idleTimeout = 20000
+    }
 
-	return &redis.Pool{
-		MaxIdle:     redisMaxIdle,
-		IdleTimeout: time.Duration(redisIdleTimeout) * time.Second,
-		Dial: func() (redis.Conn, error) {
-		dial:
-			c, err := redis.Dial("tcp", fmt.Sprintf("%s:%d", redisHost, redisPort))
-			if err != nil {
-				for {
-					log.Printf("[ Redis] - redis(%s:%d) dial failed - %s , Redial after 5 seconds\n",
-						redisHost, redisPort, err.Error())
-					time.Sleep(time.Second * 5)
-					goto dial
-				}
-			}
+    return &redis.Pool{
+        MaxIdle:     maxIdle,
+        IdleTimeout: time.Duration(idleTimeout) * time.Second,
+        Dial: func() (redis.Conn, error) {
+            var c redis.Conn
+            var err error
+            for {
+                c, err = redis.Dial("tcp", fmt.Sprintf("%s:%d", host, port))
+                if err == nil {
+                    break
+                }
+                log.Printf("[ Redis] - redis(%s:%d) dial failed - %s , Redial after 5 seconds\n",
+                    host, port, err.Error())
+                time.Sleep(time.Second * 5)
+            }
 
-			if len(redisAuth) != 0 {
-				if _, err := c.Do("AUTH", redisAuth); err != nil {
-					c.Close()
-					log.Fatalf("[ Redis][ AUTH] - %s\n", err.Error())
-				}
-			}
-			if _, err = c.Do("SELECT", redisDb); err != nil {
-				c.Close()
-				log.Fatalf("[ Redis][ SELECT] - redis(%s:%d) select db failed - %s",
-					redisHost, redisPort, err.Error())
-			}
-
-			return c, err
-		},
-		TestOnBorrow: func(c redis.Conn, t time.Time) error {
-			_, err := c.Do("PING")
-			return err
-		},
-	}
+            if len(auth) != 0 {
+                if _, err = c.Do("AUTH", auth); err != nil {
+                    c.Close()
+                    log.Fatalf("[ Redis][ AUTH] - %s\n", err.Error())
+                }
+            }
+            if _, err = c.Do("SELECT", db); err != nil {
+                c.Close()
+                log.Fatalf("[ Redis][ SELECT] - redis(%s:%d) select db failed - %s",
+                    host, port, err.Error())
+            }
+            return c, err
+        },
+        TestOnBorrow: func(c redis.Conn, t time.Time) error {
+            _, err := c.Do("PING")
+            return err
+        },
+    }
 }
 
 // 获取Redis连接池
 func GetRedisPool() *redis.Pool {
-	if !hasGet {
-		app := gof.CurrentApp
-		if app == nil {
-			panic(errors.New("gobal app not initialize!"))
-		}
-		var ok bool
-		globPool, ok = app.Storage().Driver().(*redis.Pool)
-		if !ok {
-			panic(errors.New("storage drive not base redis"))
-		}
-		hasGet = true
-	}
-	return globPool
+    if !hasGet {
+        app := gof.CurrentApp
+        if app == nil {
+            panic(errors.New("gobal app not initialize!"))
+        }
+        var ok bool
+        globPool, ok = app.Storage().Driver().(*redis.Pool)
+        if !ok {
+            panic(errors.New("storage drive not base redis"))
+        }
+        hasGet = true
+    }
+    return globPool
 }
 
 // 获取Redis连接
 func GetRedisConn() redis.Conn {
-	pool := GetRedisPool()
-	if pool != nil {
-		return pool.Get()
-	}
-	return nil
+    pool := GetRedisPool()
+    if pool != nil {
+        return pool.Get()
+    }
+    return nil
 }
 
 // 清除redis缓存
 func CleanRedisCache(app gof.App) {
-	rs := app.Storage().(storage.IRedisStorage)
-	if rs != nil {
-		i, err := rs.PrefixDel("go2o:*")
-		if err != nil {
-			log.Println("[ Go2o][ Redis][ Clean]: happend error ", err.Error())
-		} else {
-			log.Println("[ Go2o][ Redis][ Clean]: clean redis records :", i)
-		}
-	}
-	if CleanHookFunc != nil {
-		CleanHookFunc(app)
-	}
-	log.Println("[ Go2o][ Clean][ Cache]: clean ok !")
+    rs := app.Storage().(storage.IRedisStorage)
+    if rs != nil {
+        i, err := rs.PrefixDel("go2o:*")
+        if err != nil {
+            log.Println("[ Go2o][ Redis][ Clean]: happend error ", err.Error())
+        } else {
+            log.Println("[ Go2o][ Redis][ Clean]: clean redis records :", i)
+        }
+    }
+    if CleanHookFunc != nil {
+        CleanHookFunc(app)
+    }
+    log.Println("[ Go2o][ Clean][ Cache]: clean ok !")
 }
 
 // 删除指定前缀的缓存
 func RemovePrefixKeys(sto storage.Interface, prefix string) {
-	rds := sto.(storage.IRedisStorage)
-	_, err := rds.PrefixDel(prefix)
-	if err != nil {
-		log.Println("[ Cache][ Clean]: clean by prefix ", prefix, " error:", err)
-	}
+    rds := sto.(storage.IRedisStorage)
+    _, err := rds.PrefixDel(prefix)
+    if err != nil {
+        log.Println("[ Cache][ Clean]: clean by prefix ", prefix, " error:", err)
+    }
 }
