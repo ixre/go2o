9705aa7b7d0137f899419d31367937a03dbec794
jarryliu <jarrysix@gmail.com>
2016-10-02 18:04:01 +0800

    supervise

--- app/daemon/supervise.go
+++ app/daemon/supervise.go
@@ -10,139 +10,143 @@
 package daemon
 
 import (
-	"github.com/garyburd/redigo/redis"
-	"go2o/core"
-	"go2o/core/service/dps"
-	"go2o/core/variable"
-	"strconv"
-	"strings"
-	"time"
+    "github.com/garyburd/redigo/redis"
+    "go2o/core"
+    "go2o/core/service/dps"
+    "go2o/core/variable"
+    "strconv"
+    "strings"
+    "time"
 )
 
 // 监视新订单
 func superviseOrder(ss []Service) {
-	var id int
-	sv := dps.ShoppingService
-	notify := func(ss []Service) {
-		o := sv.GetSubOrder(id)
-		if o != nil {
-			for _, v := range ss {
-				if !v.OrderObs(o) {
-					break
-				}
-			}
-		}
-	}
-	// 监听队列
-	conn := core.GetRedisConn()
-	defer conn.Close()
-	for {
-		arr, err := redis.ByteSlices(conn.Do("BLPOP",
-			variable.KvOrderBusinessQueue, 0)) //取出队列的一个元素
-		if err == nil {
-			//通知订单更新
-			id, err = strconv.Atoi(string(arr[1]))
-			if err == nil {
-				go notify(ss)
-			}
-		} else {
-			appCtx.Log().Println("[ Daemon][ OrderQueue][ Error]:",
-				err.Error(), "; retry after 10 seconds.")
-			time.Sleep(time.Second * 10)
-		}
+    sv := dps.ShoppingService
+    notify := func(id int, ss []Service) {
+        o := sv.GetSubOrder(id)
+        if o != nil {
+            for _, v := range ss {
+                if !v.OrderObs(o) {
+                    break
+                }
+            }
+        }
+    }
+    // 监听队列
+    id := 0
+    conn := core.GetRedisConn()
+    defer conn.Close()
+    for {
+        arr, err := redis.ByteSlices(conn.Do("BLPOP",
+            variable.KvOrderBusinessQueue, 0)) //取出队列的一个元素
+        if err == nil {
+            //通知订单更新
+            id, err = strconv.Atoi(string(arr[1]))
+            if err == nil {
+                go notify(id, ss)
+            }
+        } else {
+            appCtx.Log().Println("[ Daemon][ OrderQueue][ Error]:",
+                err.Error(), "; retry after 10 seconds.")
+            time.Sleep(time.Second * 10)
+        }
 
-	}
+    }
 }
 
 // 监视新会员
 func superviseMemberUpdate(ss []Service) {
-	var id int
-	sv := dps.MemberService
-	notify := func(id int, action string, ss []Service) {
-		m := sv.GetMember(id)
-		if m != nil {
-			for _, v := range ss {
-				if !v.MemberObs(m, action == "create") {
-					break
-				}
-			}
-		}
-	}
-	conn := core.GetRedisConn()
-	defer conn.Close()
-	for {
-		arr, err := redis.ByteSlices(conn.Do("BLPOP",
-			variable.KvMemberUpdateQueue, 0))
-		if err == nil {
-			//通知会员修改,格式如: 1-[create|update]
-			s := string(arr[1])
-			mArr := strings.Split(s, "-")
-			id, err = strconv.Atoi(mArr[0])
-			if err == nil {
-				go notify(id, mArr[1], ss)
-			}
-		} else {
-			appCtx.Log().Println("[ Daemon][ MemberQueue][ Error]:",
-				err.Error(), "; retry after 10 seconds.")
-			time.Sleep(time.Second * 10)
-		}
-	}
+    sv := dps.MemberService
+    notify := func(id int, action string, ss []Service) {
+        m := sv.GetMember(id)
+        if m != nil {
+            for _, v := range ss {
+                if !v.MemberObs(m, action == "create") {
+                    break
+                }
+            }
+        }
+    }
+    id := 0
+    conn := core.GetRedisConn()
+    defer conn.Close()
+    for {
+        arr, err := redis.ByteSlices(conn.Do("BLPOP",
+            variable.KvMemberUpdateQueue, 0))
+        if err == nil {
+            //通知会员修改,格式如: 1-[create|update]
+            s := string(arr[1])
+            mArr := strings.Split(s, "-")
+            id, err = strconv.Atoi(mArr[0])
+            if err == nil {
+                go notify(id, mArr[1], ss)
+            }
+        } else {
+            appCtx.Log().Println("[ Daemon][ MemberQueue][ Error]:",
+                err.Error(), "; retry after 10 seconds.")
+            time.Sleep(time.Second * 10)
+        }
+    }
 }
 
 // 监视支付单完成
 func supervisePaymentOrderFinish(ss []Service) {
-	id := 0
-	sv := dps.PaymentService
-	notify := func(id int, ss []Service) {
-		order := sv.GetPaymentOrder(id)
-		if order != nil {
-			for _, v := range ss {
-				if !v.PaymentOrderObs(order) {
-					break
-				}
-			}
-		}
-	}
-	conn := core.GetRedisConn()
-	defer conn.Close()
-	for {
-		arr, err := redis.ByteSlices(conn.Do("BLPOP",
-			variable.KvPaymentOrderFinishQueue, 0))
-		if err == nil {
-			//通知服务
-			s := string(arr[1])
-			id, err = strconv.Atoi(s)
-			if err == nil {
-				go notify(id, ss)
-			}
-		} else {
-			appCtx.Log().Println("[ Daemon][ PaymentOrderQueue][ Error]:",
-				err.Error(), "; retry after 10 seconds.")
-			time.Sleep(time.Second * 10)
-			continue
-		}
-	}
+    sv := dps.PaymentService
+    notify := func(id int, ss []Service) {
+        order := sv.GetPaymentOrder(id)
+        if order != nil {
+            for _, v := range ss {
+                if !v.PaymentOrderObs(order) {
+                    break
+                }
+            }
+        }
+    }
+    id := 0
+    conn := core.GetRedisConn()
+    defer conn.Close()
+    for {
+        arr, err := redis.ByteSlices(conn.Do("BLPOP",
+            variable.KvPaymentOrderFinishQueue, 0))
+        if err == nil {
+            //通知服务
+            s := string(arr[1])
+            id, err = strconv.Atoi(s)
+            if err == nil {
+                go notify(id, ss)
+            }
+        } else {
+            appCtx.Log().Println("[ Daemon][ PaymentOrderQueue][ Error]:",
+                err.Error(), "; retry after 10 seconds.")
+            time.Sleep(time.Second * 10)
+        }
+    }
 }
 
 // 检测已过期的订单并标记
 func detectOrderExpires() {
-	conn := core.GetRedisConn()
-	defer conn.Close()
-	//获取标记为等待过期的订单
-	orderId := 0
-	list, _ := redis.Strings(conn.Do("KEYS", variable.KvOrderExpiresTime+"*"))
-	ss := dps.ShoppingService
-	for _, v := range list {
-		if unix, err := redis.Int64(conn.Do("GET", v)); err == nil {
-			//获取过期时间
-			if unix < time.Now().Unix() {
-				//订单号
-				orderId, err = strconv.Atoi(v[len(variable.KvOrderExpiresTime):])
-				err = ss.CancelOrder(orderId, "订单超时,自动取消")
-				//清除待取消记录
-				conn.Do("DEL", v)
-				//log.Println("---",orderId,"---",unix, "--", time.Now().Unix(), v, err)
-			}
-		}
-	}
+    conn := core.GetRedisConn()
+    defer conn.Close()
+    //获取标记为等待过期的订单
+    orderId := 0
+    list, _ := redis.Strings(conn.Do("KEYS", variable.KvOrderExpiresTime + "*"))
+    ss := dps.ShoppingService
+    for _, v := range list {
+        unix, err := redis.Int64(conn.Do("GET", v))
+        if err == nil {
+            //获取过期时间
+            if unix < time.Now().Unix() {
+                //订单号
+                orderId, err = strconv.Atoi(v[len(variable.KvOrderExpiresTime):])
+                err = ss.CancelOrder(orderId, "订单超时,自动取消")
+                //清除待取消记录
+                conn.Do("DEL", v)
+                //log.Println("---",orderId,"---",unix, "--", time.Now().Unix(), v, err)
+            }
+        } else {
+            appCtx.Log().Println("[ Daemon][ Order][ Cancel][ Error]:",
+                err.Error(), "; retry after 10 seconds.")
+            time.Sleep(time.Second * 10)
+        }
+    }
 }
