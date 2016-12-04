42d0767f1c861b2a8d1142107e715112e78dc5a9
jarryliu <jarrysix@gmail.com>
2016-12-04 14:37:05 +0800

    account refactor
    Former-commit-id: 94f84a6b012758120e656201aa86d7d03fba9e5c

--- core/service/rsi/personfinance_service.go
+++ core/service/rsi/personfinance_service.go
@@ -120,7 +120,8 @@ func (p *personFinanceService) RiseTransferOut(personId int32,
 	if transferWith == personfinance.TransferOutWithBalance {
 		//转入余额
 		if err = r.TransferOut(amount, transferWith, personfinance.RiseStateOk); err == nil {
-			err = acc.ChargeForBalance(member.ChargeBySystem, "理财转出",
+			err = acc.Charge(member.AccountBalance,
+				member.KindBalanceServiceCharge, "理财转出",
 				domain.NewTradeNo(10000), amount, member.DefaultRelateUser)
 			if err != nil {
 				log.Println("[ TransferOut][ Error]:", err.Error())
