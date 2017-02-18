707fb8b91d7fe0ecaa1488c4f25613a4ee28b5df
jarryliu <jarrysix@gmail.com>
2017-02-18 18:17:39 +0800

    go2o-publish

--- docs/data/upgrade_v3.sql
+++ docs/data/upgrade_v3.sql
@@ -414,7 +414,7 @@ CHANGE COLUMN `total_present_amount` `total_wallet_amount` DECIMAL(10,2) NOT NUL
 ALTER TABLE `mm_present_log`
 RENAME TO  `mm_wallet_log` ;
 
-ALTER TABLE `txmall`.`gs_sales_snapshot`
-RENAME TO  `txmall`.`item_trade_snapshot` ;
+ALTER TABLE `gs_sales_snapshot`
+RENAME TO  `item_trade_snapshot` ;
 
-DROP TABLE `txmall`.`gs_sale_snapshot`;
+DROP TABLE `gs_sale_snapshot`;
