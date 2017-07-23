ea03b8d3b4ca3860651522a75be0b7ad36c9cdfe
jarryliu <jarrysix@gmail.com>
2017-07-23 11:33:11 +0800

    bash shell
    Former-commit-id: c28a154524e0b8c07b422b596be0dc231240fc00

--- go2o-publish.sh
+++ go2o-publish.sh
@@ -9,15 +9,14 @@
  ###   ###   ###   ###
 
 
-package_name="go2o_deploy_tmp.tar.gz"
-
 goos=linux
 arch=amd64
+env="0"
 server="127.0.0.1"
+ssh_port=22
 user=root
 root_user=root
-ssh_port=22
-env="0"
+app_dir="/data/www/go2o/"
 
 echo "---------------------------"
 echo "*** GO2O PUBLISH SCRIPT ***"
@@ -27,6 +26,10 @@ echo " 1): Development"
 echo " 2): Nightly & Beta"
 echo " 3): Release"
 
+package_name="update.tar.gz"
+zip_bin="go2o-serve go2o-tcpserve master-serve go2o-rpc"
+zip_res="public mobile conf/query uploads/res go2o.sh"
+boot_sh="sudo ./go2o.sh restart"
 
 # 获取发布环境
 while true
@@ -35,7 +38,7 @@ do
    echo -n "Please input : "
    # 读取发布环境
    read env
-    if [ ${env} = "1" ] || [ ${env} = "2" ] || [ ${env} = "3" ];then
+    if [[ ${env} = "1" ]] || [[ ${env} = "2" ]] || [[ ${env} = "3" ]];then
         break
     else
         echo "Please retype correct index:"
@@ -43,41 +46,39 @@ do
 done
 
 # 开发环境
-if [ ${env} = "1" ];then
+if [[ ${env} = "1" ]];then
     arch=arm
     server="dev.go2o.to2.net"
     user=pi
     root_user=pi
     ssh_port=22
-    app_dir="/home/${user}/www/flm-dev/"
+    app_dir="/data/www/go2o"
     echo "Selected : Development"
 fi
 
 # 测试或每夜环境
-if [ ${env} = "2" ];then
+if [[ ${env} = "2" ]];then
      server="192.168.4.201"
      user=flm
      root_user=flm
-     app_dir="/home/${user}/www/flm/"
+     app_dir="/data/www/go2o"
      echo "Selected : Nightly & Beta"
 fi
 
 # 发布版
-if [ ${env} = "3" ];then
+if [[ ${env} = "3" ]];then
      server="official.go2o.to2.net"
      user=flm
-     app_dir="/home/${user}/www/flm/"
+     app_dir="/data/www/go2o"
      echo "Selected : Release"
 fi
-
-boot_sh="cd ${app_dir} &&tar xvzf /home/${user}/${package_name} && sudo ./go2o.sh restart"
 
 echo ""
 echo "[ Setup 1 ]: compile program "
 echo "Please confirm compile : [Y/N]"
 
 read compile
-if [ ${compile} = "Y" ]||[ ${compile} = "y" ];then
+if [[ ${compile} = "Y" ]] || [[ ${compile} = "y" ]];then
     echo "compiling ..."
     CGO_ENABLED=0 GOOS=${goos} GOARCH=${arch} go build -ldflags "-w" go2o-serve.go
     CGO_ENABLED=0 GOOS=${goos} GOARCH=${arch} go build -ldflags "-w" go2o-tcpserve.go
@@ -86,42 +87,44 @@ if [ ${compile} = "Y" ]||[ ${compile} = "y" ];then
     #CGO_ENABLED=0 GOOS=${goos} GOARCH=${arch} go build -ldflags "-w" merchant-serve.go
     #CGO_ENABLED=0 GOOS=${goos} GOARCH=${arch} go build -ldflags "-w" pub-serve.go
 else
-    echo "  skipping compile"
+    echo "  skipping compile" && echo ""
 fi
-
-
 
 echo "[ Setup 2 ]: zipping tar package ..."
 sleep 1
 
-zipRes="go2o-serve go2o-tcpserve master-serve public mobile conf/query uploads/res go2o.sh"
 
 echo "Include ""conf"" folder [Y/N] ? "
 read zipConf
 
-if [ ${zipConf} = "Y" ]||[ ${zipConf} = "y" ];then
-    zipRes=${zipRes}" conf/core"
+if [[ ${zipConf} = "Y" ]] || [[ ${zipConf} = "y" ]];then
+    zip_res=${zip_res}" conf/core"
+else
+    echo "  skipping conf folder" && echo ""
 fi
 
 echo "Include ""static"" folder [Y/N] ? "
 read zipStatic
-if [ ${zipStatic} = "Y" ]||[ ${zipStatic} = "y" ];then
-    zipRes=${zipRes}" static"
+if [[ ${zipStatic} = "Y" ]] || [[ ${zipStatic} = "y" ]];then
+    zip_res=${zip_res}" static"
+else
+    echo "  skipping static folder" && echo ""
 fi
 
-echo ${zipRes}
-tar cvzf ../${package_name} ${zipRes}
+zip_res=${zip_bin}" "${zip_res}
+echo ${zip_res}
+tar cvzf ../${package_name} ${zip_res}
+
 
 echo "[ Setup 3 ]: upload tar package to server ..."
 
-scp ../${package_name} ${user}@${server}:/home/${user}/
+scp ../${package_name} ${user}@${server}:${app_dir}
 
 echo "[ Setup 4 ]: restart server"
-
-ssh -t -p ${ssh_port} ${root_user}@${server} "${boot_sh}"
+ssh -t -p ${ssh_port} ${root_user}@${server} "cd ${app_dir} && tar xvzf ${package_name} && ${boot_sh}"
 
 echo "[ Setup 5 ]: cleaning ..."
-rm go2o-serve go2o-tcpserve master-serve go2o-rpc ../${package_name}
+rm  ${zip_bin} ../${package_name}
 
 echo "Configurations, publish successfully!"
 
