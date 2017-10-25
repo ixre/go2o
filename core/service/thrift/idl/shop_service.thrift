2948955a22174ba23ad92dad0a5f71ed260d9a6a
jarryliu <jarrysix@gmail.com>
2017-10-26 04:37:01 +0800

    thrift update
    Former-commit-id: fe5395620869f10df972f0dae590c69958925b4c

---
+++ core/service/thrift/idl/shop_service.thrift
@@ -0,0 +1,43 @@
+namespace go define
+
+include "type.thrift"
+
+// 商店,需重构
+struct Shop{
+    1:i32 ID
+    2:i32 VendorId
+    3:i32 ShopType
+    4:string Name
+    5:i32 State
+    6:i32 OpeningState
+    7:map<string,string> Data
+}
+
+// 商铺
+struct Store{
+    1:i32 ID
+    2:i32 VendorId
+    3:string Name
+    4:string Alias
+    5:string Host
+    6:string Logo
+    7:i32 State
+    8:i32 OpeningState
+    9:string StorePhone
+    10:string StoreTitle
+    11:string StoreNotice
+}
+
+// 商店服务
+service ShopService{
+    // 获取店铺
+    Store GetStore(1:i32 venderId)
+    // 获取店铺
+    Store GetStoreById(1:i32 shopId)
+    // 获取门店
+    //Shop GetOfflineShop(1:i32 shopId)
+    // 打开或关闭商店
+    type.Result TurnShop(1:i32 shopId,2:bool on,3:string reason)
+    // 设置商店是否营业
+    type.Result OpenShop(1:i32 shopId,2:bool opening,3:string reason)
+}
---
+++ core/service/thrift/idl/type.thrift
@@ -0,0 +1,204 @@
+namespace go define
+
+//传输结果对象
+struct Result{
+   1:i32 ID
+   2:bool Result
+   3:string Code
+   4:string Message
+}
+//传输结果对象
+struct Result64{
+   1:i64 ID
+   2:bool Result
+   3:string Code
+   4:string Message
+}
+//传输结果对象(Double)
+struct DResult{
+   1:double Data
+   2:bool Result
+   3:string Code
+   4:string Message
+}
+
+// 键值对
+struct Pair{
+   1:string Key
+   2:string Value
+}
+
+
+
+//商品分类
+struct Category {
+    1: i32 ID
+    2: i32 ParentId
+    3: i32 ProModel
+    // 分类优先级
+    4: i32 Priority
+    // 分类名称
+    5: string Name
+    // 虚拟分类
+    6: i32 VirtualCat
+    // 分类目标地址
+    7: string CatUrl
+    8: string Icon
+    // 图标（雪碧图）坐标
+    9: string IconXY
+    10: i32 SortNum
+    11: i32 FloorShow
+    12: i32 Enabled
+    13: i32 Level
+    14: i64 CreateTime
+    15: list<Category> Children
+}
+
+
+struct Sku {
+    1: i64 SkuId
+    2: i64 ItemId
+    3: i64 ProductId
+    4: string Title
+    5: string Image
+    6: string SpecData
+    7: string SpecWord
+    8: string Code
+    9: double RetailPrice
+    10: double Price
+    11: double Cost
+    12: i32 Weight
+    13: i32 Bulk
+    14: i32 Stock
+    15: i32 SaleNum
+}
+
+
+struct OldItem{
+    1: i64 ItemId
+    2: i64 ProductId
+    3: i32 PromFlag
+    4: i32 CatId
+    5: i32 VendorId
+    6: i32 BrandId
+    7: i32 ShopId
+    8: i32 ShopCatId
+    9: i32 ExpressTid
+    10: string Title
+    11: string ShortTitle
+    12: string Code
+    13: string Image
+    14: i32 IsPresent
+    15: string PriceRange
+    16: i32 StockNum
+    17: i32 SaleNum
+    18: i32 SkuNum
+    19: i64 SkuId
+    20: double Cost
+    21: double Price
+    22: double RetailPrice
+    23: i32 Weight
+    24: i32 Bulk
+    25: i32 ShelveState
+    26: i32 ReviewState
+    27: string ReviewRemark
+    28: i32 SortNum
+    29: i64 CreateTime
+    30: i64 UpdateTime
+    31: double PromPrice
+    32: list<Sku> SkuArray
+    33: map<string,string> Data;
+}
+
+// 统一的商品对象
+struct UnifiedItem{
+    1: i64 ItemId
+    2: i32 ProductId
+    3: i32 CatId
+    4: i32 VendorId
+    5: i32 BrandId
+    6: string Title
+    7: string Code
+    8: string Image
+    9: double Price
+    10: string PriceRange
+    11: i32 StockNum
+    12: i32 ShelveState
+    13: i32 ReviewState
+    14: i64 UpdateTime
+    15: list<Sku> SkuArray
+    16: map<string,string> Data
+    // 3: i32 PromFlag
+    // 7: i32 ShopId
+    // 8: i32 ShopCatId
+    // 9: i32 ExpressTid
+    // 11: string ShortTitle
+    // 14: i32 IsPresent
+    // 23: i32 Weight
+    // 24: i32 Bulk
+    // 18: i32 SkuNum
+    // 28: i32 SortNum
+    // 29: i64 CreateTime
+    // 31: double PromPrice
+    // 22: double RetailPrice
+    // 19: i32 SkuId
+    // 20: double Cost
+    // 17: i32 SaleNum
+
+}
+
+
+
+// 购物车
+struct ShoppingCart {
+    //编号
+    1: i32 CartId
+    //购物车KEY
+    2: string Code
+    //店铺分组
+    3: list<ShoppingCartGroup> Shops
+}
+// 购物车商铺分组
+struct ShoppingCartGroup {
+    //商铺编号
+    1: i32 ShopId
+    //供货商编号
+    2: i32 VendorId
+    //商铺名称
+    3: string ShopName
+    //是否结算
+    4: bool Checked
+    //商品
+    5: list<ShoppingCartItem> Items
+}
+
+
+// 购物车商品
+struct ShoppingCartItem {
+    //商品编号
+    1: i64 ItemId
+    //SKU编号
+    2: i64 SkuId
+    //商品标题
+    3: string Title
+    //商品图片
+    4: string Image
+    //规格文本
+    5: string SpecWord
+    //商品编码
+    6: string Code
+    //零售价
+    7: double RetailPrice
+    //销售价
+    8: double Price
+    //数量
+    9: i32 Quantity
+    //是否结算
+    10: bool Checked
+    //库存文本
+    11: string StockText
+    //店铺编号
+    12: i32 ShopId
+}
+
+
