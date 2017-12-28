b3e52f44faa796f4dcadfffe3fb755b582f89e86
jarryliu <jarrysix@gmail.com>
2017-12-28 22:43:47 +0800

    merchant service

---
+++ core/service/thrift/parser/merchant_dto.go
@@ -0,0 +1,30 @@
+package parser
+
+import (
+	"go2o/core/domain/interface/merchant"
+	"go2o/gen-code/thrift/define"
+)
+
+func TradeConfDto(conf *merchant.TradeConf) *define.STradeConf {
+	return &define.STradeConf{
+		MchId:       conf.MchId,
+		TradeType:   int32(conf.TradeType),
+		PlanId:      conf.PlanId,
+		Flag:        int32(conf.Flag),
+		AmountBasis: int32(conf.AmountBasis),
+		TradeFee:    int32(conf.TradeFee),
+		TradeRate:   int32(conf.TradeRate),
+	}
+}
+
+func TradeConf(conf *define.STradeConf) *merchant.TradeConf {
+	return &merchant.TradeConf{
+		MchId:       conf.MchId,
+		TradeType:   int(conf.TradeType),
+		PlanId:      conf.PlanId,
+		Flag:        int(conf.Flag),
+		AmountBasis: int(conf.AmountBasis),
+		TradeFee:    int(conf.TradeFee),
+		TradeRate:   int(conf.TradeRate),
+	}
+}
