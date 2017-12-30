977434125f0f501708e181bb0802efa9490c04dc
jarryliu <jarrysix@gmail.com>
2017-12-31 00:33:10 +0800

    upgrade and version

--- core/domain/merchant/sale_manager.go
+++ core/domain/merchant/sale_manager.go
@@ -1,6 +1,9 @@
 package merchant
 
-import "go2o/core/domain/interface/merchant"
+import (
+	"go2o/core/domain/interface/enum"
+	"go2o/core/domain/interface/merchant"
+)
 
 var _ merchant.ISaleManager = new(SaleManagerImpl)
 
@@ -16,6 +19,27 @@ func newSaleManagerImpl(id int, m *merchantImpl) merchant.ISaleManager {
 	}
 }
 
+// 计算交易手续费
 func (s *SaleManagerImpl) MathTradeFee(tradeType int, amount int) (int, error) {
-	panic("implement me")
+	cm := s.mch.ConfManager()
+	conf := cm.GetTradeConf(tradeType)
+	if conf == nil {
+		//todo: 应使用系统默认的比例进行手续费
+		return 0, nil
+	}
+	// 免费
+	if conf.Flag&merchant.TFlagFree == merchant.TFlagFree {
+		return 0, nil
+	}
+	switch conf.AmountBasis {
+	case enum.AmountBasisNotSet: // 免费
+		return 0, nil
+	case enum.AmountBasisByAmount: // 按订单单数，收取金额
+		return conf.TradeFee, nil
+	case enum.AmountBasisByPercent: // 按订单金额，收取百分比
+		return int(float64(amount*conf.TradeRate) / enum.RATE_Percent), nil
+	default:
+		panic("not support amount basis")
+	}
+	return 0, nil
 }
--- docs/data/upgrade_v0.1.0.sql
+++ docs/data/upgrade_v0.1.0.sql
@@ -877,15 +877,3 @@ CREATE TABLE wal_wallet_log (
   create_time   int(11) NOT NULL comment '创建时间',
   update_time   int(11) NOT NULL comment '更新时间',
   PRIMARY KEY (id)) comment='钱包日志';
-
-CREATE TABLE mch_trade_conf (
-  id          int(10) NOT NULL AUTO_INCREMENT comment '编号',
-  mch_id      int(10) NOT NULL comment '商户编号',
-  trade_type  int(1) NOT NULL comment '交易类型',
-  plan_id     int(10) NOT NULL comment '交易方案，根据方案来自动调整比例',
-  flag        int(4) NOT NULL comment '交易标志',
-  basis       int(1) NOT NULL comment '交易手续费依据,1:按金额 2:按比例',
-  trade_fee   int(4) NOT NULL comment '交易费，按单笔收取',
-  trade_rate  int(4) NOT NULL comment '交易手续费比例',
-  update_time int(11) NOT NULL comment '更新时间',
-  PRIMARY KEY (id)) comment='商户交易设置';
