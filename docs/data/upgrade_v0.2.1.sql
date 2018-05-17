0adc5f9db09ea5234d076f887d058c2cce6e1e56
jarryliu <jarrysix@gmail.com>
2018-05-17 09:03:01 +0800

    合并支付单

--- docs/data/upgrade_v0.2.1.sql
+++ docs/data/upgrade_v0.2.1.sql
@@ -1,3 +1,19 @@
+CREATE TABLE pay_sp_trade (
+  id           int(10) NOT NULL AUTO_INCREMENT,
+  trade_sp     varchar(20) NOT NULL comment '交易SP',
+  trade_no     varchar(45) NOT NULL comment '交易号',
+  trade_orders varchar(512) NOT NULL comment '合并的订单号,交易号用"|"分割',
+  trade_state  int(1) NOT NULL comment '交易状态',
+  trade_result int(1) NOT NULL comment '交易结果',
+  trade_remark varchar(200) NOT NULL comment '交易备注',
+  trade_time   int(11) NOT NULL comment '交易时间',
+  PRIMARY KEY (id));
+CREATE TABLE pay_merge_order (
+  id             int(10) NOT NULL AUTO_INCREMENT comment '编号',
+  merge_trade_no varchar(45) NOT NULL comment '合并交易单号',
+  merge_orders   varchar(512) NOT NULL comment '合并的订单号,交易号用"|"分割',
+  submit_time    int(11) NOT NULL comment '提交时间',
+  PRIMARY KEY (id)) comment='合并的支付单';
 CREATE TABLE pay_trade_chan (
   id            int(10) NOT NULL AUTO_INCREMENT comment '编号',
   trade_no      varchar(45) NOT NULL comment '交易单号',
@@ -29,25 +45,14 @@ CREATE TABLE pay_order (
   deduct_amount   int(10) NOT NULL comment '抵扣金额 ',
   procedure_fee   int(10) NOT NULL comment '手续费',
   final_fee       int(10) NOT NULL comment '最终支付金额',
-  pay_flag        int(6) NOT NULL comment '可⽤支付方式 ',
+  pay_flag        int(4) NOT NULL comment '可⽤支付方式 ',
   extra_data      varchar(120) NOT NULL comment '其他支付信息',
   trade_channel   int(2) NOT NULL comment '交易支付渠道',
   out_trade_sp    varchar(10) NOT NULL comment '外部交易提供商',
   out_trade_no    varchar(45) NOT NULL comment '外部交易订单号',
-  merge_trade_no  varchar(45) NOT NULL comment '合并支付交单单号',
-  payment_sign    int(1) NOT NULL comment '可作废',
   state           int(1) NOT NULL comment '订单状态',
   submit_time     int(11) NOT NULL comment '提交时间',
   expires_time    int(11) NOT NULL comment '过期时间',
   paid_time       int(11) NOT NULL comment '支付时间',
   update_time     int(11) NOT NULL comment '更新时间',
   PRIMARY KEY (id)) comment='支付单';
-
-ALTER TABLE `mm_trusted_info`
-CHANGE COLUMN `reviewed` `review_state` TINYINT(1) NULL DEFAULT NULL;
-ALTER TABLE `mm_levelup`
-CHANGE COLUMN `reviewed` `review_state` TINYINT(1) NULL DEFAULT NULL;
-ALTER TABLE `mch_enterprise_info`
-CHANGE COLUMN `reviewed` `review_state` TINYINT(1) NULL DEFAULT NULL;
-
-
