81ae957fd285aa8a3c921fe75fa899e31455dd6f
jarryliu <jarrysix@gmail.com>
2018-07-24 09:12:20 +0800

    uams

--- app/uams/uams.go
+++ app/uams/uams.go
@@ -1,89 +1,285 @@
 package uams
 
 import (
+	"encoding/json"
 	"errors"
-	"fmt"
 	"github.com/jsix/gof/api"
-	"io/ioutil"
-	"net/http"
-	"net/url"
+	"github.com/jsix/gof/web/ui/tree"
 	"strconv"
-	"strings"
 )
 
 var (
-	RPermissionDenied = &api.Response{
-		ErrCode: api.RAccessDenied.ErrCode,
+	RInternalError = &api.Response{
+		RspCode: api.RInternalError.RspCode,
+		ErrMsg:  "内部服务器出错",
+	}
+	RAccessDenied = &api.Response{
+		RspCode: api.RAccessDenied.RspCode,
 		ErrMsg:  "没有权限访问该接口",
 	}
-	RMissingApiParams = &api.Response{
-		ErrCode: api.RIncorrectApiParams.ErrCode,
+	RIncorrectApiParams = &api.Response{
+		RspCode: api.RIncorrectApiParams.RspCode,
 		ErrMsg:  "缺少接口参数，请联系技术人员解决",
 	}
-	RErrApiName = &api.Response{
-		ErrCode: api.RErrUndefinedApi.ErrCode,
+	RUndefinedApi = &api.Response{
+		RspCode: api.RUndefinedApi.RspCode,
 		ErrMsg:  "调用的API名称不正确",
 	}
 	RNoSuchApp = &api.Response{
-		ErrCode: 10096,
+		RspCode: 10096,
 		ErrMsg:  "no such app",
 	}
 )
 
-var (
-	API_SERVER    = "http://localhost:1419/uams_api_v1"
-	API_USER      = "< replace your api user >"
-	API_TOKEN     = "< replace your api token >"
-	API_APP       = "< replace your serve code >"
-	API_SIGN_TYPE = "sha1" // [sha1|md5]
-)
+type Client struct {
+	*api.Client
+}
+
+func NewCli(server, key, secret, signType string) *Client {
+	return &Client{
+		Client: api.NewClient(server, key, secret, signType, checkApiRespErr),
+	}
+}
 
 // 请求接口
-func Post(apiName string, data map[string]string) ([]byte, error) {
-	cli := &http.Client{}
-	form := url.Values{}
-	if data != nil {
-		for k, v := range data {
-			form[k] = []string{v}
-		}
-	}
-	form["api"] = []string{apiName}
-	form["key"] = []string{API_USER}
-	form["sign_type"] = []string{API_SIGN_TYPE}
-	form["app"] = []string{API_APP}
-	form["version"] = []string{"1.2.0.100"}
-	sign := api.Sign(API_SIGN_TYPE, form, API_TOKEN)
-	form["sign"] = []string{sign}
-	rsp, err := cli.PostForm(API_SERVER, form)
+func (c *Client) Post(apiName string, data map[string]string) ([]byte, error) {
+	data["version"] = "1.0.0"
+	return c.Client.Post(apiName, data)
+}
+
+// 用户登陆，返回user_id和user_code,real_name,user_state
+func (c *Client) UserLogin(user string, pwd string) (map[string]string, error) {
+	data, err := c.Post("user.login", map[string]string{
+		"user": user,
+		"pwd":  pwd,
+	})
 	if err == nil {
-		if rsp.StatusCode != http.StatusOK {
-			return []byte{}, errors.New(fmt.Sprintf("Http %d", rsp.StatusCode))
+		var r Result
+		json.Unmarshal(data, &r)
+		if r.ErrCode != 0 {
+			err = errors.New(r.ErrMsg)
 		}
-		data, err := ioutil.ReadAll(rsp.Body)
-		if err == nil {
-			content := string(data)
-			if strings.HasPrefix(content, "!") {
-				arr := strings.Split(content[1:], ":")
-				code, _ := strconv.Atoi(arr[0])
-				return data, checkApiRespErr(code, arr[1])
-			}
-			return data, nil
+		return r.Data, err
+	}
+	return nil, err
+}
+
+// 获取所有部门
+func (c *Client) GetDeparts(app string) ([]*UamsDepart, error) {
+	var d []*UamsDepart
+	r, err := c.Post("dept.all", map[string]string{
+		"app": app,
+	})
+	if err == nil {
+		err = json.Unmarshal([]byte(r), &d)
+	}
+	return d, err
+}
+
+// 获取部门树
+func (c *Client) GetDepartTree(app string) (*tree.TreeNode, error) {
+	d := tree.TreeNode{}
+	r, err := c.Post("dept.tree", map[string]string{
+		"app": app,
+	})
+	if err == nil {
+		err = json.Unmarshal([]byte(r), &d)
+	}
+	return &d, err
+}
+
+// 获取角色
+func (c *Client) GetRoles(app string) ([]*Role, error) {
+	var d []*Role
+	r, err := c.Post("role.all", map[string]string{
+		"app": app,
+	})
+	if err == nil {
+		err = json.Unmarshal([]byte(r), &d)
+	}
+	return d, err
+}
+
+// 是否匹配部门
+func (c *Client) MatchDept(app string, outerUid string, dept int64) error {
+	r, err := c.Post("dept.match", map[string]string{
+		"outer_uid": outerUid,
+		"dept":      strconv.Itoa(int(dept)),
+		"app":       app,
+	})
+	if err == nil {
+		b, _ := strconv.ParseBool(string(r))
+		if !b {
+			err = errors.New("not match")
 		}
 	}
-	return []byte{}, err
+	return err
+}
+
+// 是否匹配角色
+func (c *Client) MatchRole(app string, outerUid string, role int64) error {
+	r, err := c.Post("role.match", map[string]string{
+		"outer_uid": outerUid,
+		"role":      strconv.Itoa(int(role)),
+		"app":       app,
+	})
+	if err == nil {
+		b, _ := strconv.ParseBool(string(r))
+		if !b {
+			err = errors.New("not match")
+		}
+	}
+	return err
+}
+
+// 插件是否能权限访问资源
+func (c *Client) CheckPrivilege(app string, userCode string, resCode string, resUri string) error {
+	_, err := c.Post("user.privilege", map[string]string{
+		"app":      app,
+		"user_code":  userCode,
+		"res_code": resCode,
+		"res_uri":  resUri,
+	})
+	return err
 }
 
 // 如果返回接口请求错误, 响应状态码以-10开头
 func checkApiRespErr(code int, text string) error {
-	switch int64(code) {
-	case api.RAccessDenied.ErrCode:
-		text = RPermissionDenied.ErrMsg
-	case api.RIncorrectApiParams.ErrCode:
-		text = RMissingApiParams.ErrMsg
-	case api.RErrUndefinedApi.ErrCode:
-		text = RErrApiName.ErrMsg
-	case RNoSuchApp.ErrCode:
+	switch code {
+	case api.RAccessDenied.RspCode:
+		text = RAccessDenied.ErrMsg
+	case api.RIncorrectApiParams.RspCode:
+		text = RIncorrectApiParams.ErrMsg
+	case api.RUndefinedApi.RspCode:
+		text = RUndefinedApi.ErrMsg
+	case RNoSuchApp.RspCode:
 		text = RNoSuchApp.ErrMsg
 	}
 	return errors.New(text)
 }
+
+type (
+	Result struct {
+		ErrCode int32             `thrift:"RspCode,1" db:"RspCode" json:"RspCode"`
+		ErrMsg  string            `thrift:"ErrMsg,2" db:"ErrMsg" json:"ErrMsg"`
+		Data    map[string]string `thrift:"Data,3" db:"Data" json:"Data"`
+	}
+
+	// 菜单
+	Res struct {
+		ID       int64
+		Title    string
+		Code     string
+		URI      string
+		Data     map[string]string
+		Children []*Res
+	}
+
+	// 资源
+	UamsResource struct {
+		// ID
+		ID int `db:"id" pk:"yes" auto:"yes"`
+		// 上级资源
+		ParentId int `db:"parent_id"`
+		// 应用编号
+		AppId int `db:"app_id"`
+		// 资源标题
+		ResTitle string `db:"res_title"`
+		// 资源类型，如menu/button
+		ResType string `db:"res_type"`
+		// 资源编码
+		ResCode string `db:"res_code"`
+		// 资源权限可选值，用"|"分割
+		ResOption string `db:"res_option"`
+		// 资源是否单独开放给用户
+		ResOpen int32 `db:"res_open"`
+		// 资源图标
+		ResIcon string `db:"-"`
+		// 资源URL
+		ResUri string `db:"res_uri"`
+		// 资源数据
+		ResData string `db:"res_data"`
+		// 资源状态
+		ResState int32 `db:"res_state"`
+	}
+
+	// 资源
+	ResourceTreeNode struct {
+		// ID
+		ID int `json:"id"`
+		// 资源标题
+		Title string `json:"title"`
+		// 资源类型，如menu/button
+		Type string `json:"type"`
+		// 资源编码
+		Code string `json:"code"`
+		// 图标
+		Icon string `json:"icon"`
+		// 资源URL
+		Uri string `json:"uri"`
+		// 子资源
+		Children []*ResourceTreeNode `json:"children"`
+	}
+
+	// 部门
+	UamsDepart struct {
+		// 部门ID
+		ID int64 `db:"id" pk:"yes" auto:"yes"`
+		// 应用编号
+		AppId int64 `db:"app_id"`
+		// 上级部门编号
+		DeptId int64 `db:"dept_id"`
+		// 部门名称
+		DeptName string `db:"dept_name"`
+		// 部门编码
+		DeptCode string `db:"dept_code"`
+		// 部门描述
+		DeptDesc string `db:"dept_desc"`
+		// 子部门
+		Children []*UamsDepart `db:"-"`
+	}
+
+	// 用户字段
+	UamsUserField struct {
+		// 字段ID
+		ID int64 `db:"id" pk:"yes" auto:"yes"`
+		// 应用编号
+		AppId int64 `db:"app_id"`
+		// 字段名称
+		FieldName string `db:"field_name"`
+		// 字段编码
+		FieldCode string `db:"field_code"`
+		// 字段类型
+		FieldType string `db:"field_type"`
+		// 字段可选值
+		FieldOption string `db:"field_option"`
+		// 字段默认值
+		FieldDefault string `db:"field_default"`
+		// 字段长度
+		FieldLen int64 `db:"field_len"`
+		// 字段是否必填
+		FieldRequire int32 `db:"field_require"`
+		// 排序序号
+		SortNum int64 `db:"sort_num"`
+		// 字段状态
+		FieldState int32 `db:"field_state"`
+	}
+
+	// 角色
+	Role struct {
+		// 角色编号
+		ID int `db:"id" pk:"yes" auto:"yes"`
+		// 应用编号
+		AppId int `db:"app_id"`
+		// 角色名称
+		RoleName string `db:"role_name"`
+		// 角色位值
+		RoleFlag int `db:"role_flag"`
+		// 角色描述
+		RoleDesc string `db:"role_desc"`
+		// 默认角色
+		IsDefault int `db:"is_default"`
+		// 是否启用
+		Enabled int `db:"enabled"`
+	}
+)
