2aa11b77d4518ce8bac69464e80782b49b0d358d
jarrysix <jarrysix@gmail.com>
2019-09-06 05:18:35 +0800

    go2o tests

--- core/domain/mss/mss_test.go
+++ core/domain/mss/mss_test.go
@@ -14,7 +14,6 @@ import (
 	"github.com/ixre/gof/db/orm"
 	"go2o/core/domain/interface/mss"
 	"go2o/core/domain/interface/mss/notify"
-	_ "go2o/tests"
 	"testing"
 )
 
@@ -25,6 +24,27 @@ type MssRepo struct {
 	_globMss     mss.IUserMessageManager
 	_notifyItems map[string]*notify.NotifyItem
 	_sysManger   mss.IMessageManager
+}
+
+func (this *MssRepo) SaveConfig(userId int32, conf *mss.Config) error {
+	panic("implement me")
+}
+
+
+func (this *MssRepo) MessageManager() mss.IMessageManager {
+	panic("implement me")
+}
+
+func (this *MssRepo) NotifyManager() notify.INotifyManager {
+	panic("implement me")
+}
+
+func (this *MssRepo) GetMessageContent(msgId int32) *mss.Content {
+	panic("implement me")
+}
+
+func (this *MssRepo) GetMessageTo(msgId int32, toUserId int32, toRole int) *mss.To {
+	panic("implement me")
 }
 
 func NewMssRepo(conn db.Connector) mss.IMssRepo {
@@ -53,10 +73,6 @@ func (this *MssRepo) GetConfig(userId int32) *mss.Config {
 	return nil
 }
 
-// 保存消息设置
-func (this *MssRepo) SaveConfig(userId int, conf *mss.Config) error {
-	return nil
-}
 
 // 获取所有的通知项
 func (this *MssRepo) GetAllNotifyItem() []notify.NotifyItem {
@@ -73,8 +89,8 @@ func (this *MssRepo) SaveNotifyItem(v *notify.NotifyItem) error {
 	return nil
 }
 
-// 获取邮箱模板
-func (this *MssRepo) GetMailTemplate(mchId, id int) *mss.MailTemplate {
+
+func (this *MssRepo) GetMailTemplate(userId, id int32) *mss.MailTemplate {
 	var e mss.MailTemplate
 	if err := this._conn.GetOrm().Get(id, &e); err == nil {
 		return &e
@@ -82,19 +98,19 @@ func (this *MssRepo) GetMailTemplate(mchId, id int) *mss.MailTemplate {
 	return nil
 }
 
-// 保存邮箱模版
-func (this *MssRepo) SaveMailTemplate(v *mss.MailTemplate) (int32, error) {
-	return v.Id, nil
+func (this *MssRepo) SaveMailTemplate(*mss.MailTemplate) (int32, error) {
+	panic("implement me")
 }
+
+func (this *MssRepo) DeleteMailTemplate(userId, id int32) error {
+	//return v.Id, nil
+	return nil
+}
+
 
 // 获取所有的邮箱模版
 func (this *MssRepo) GetMailTemplates(mchId int32) []*mss.MailTemplate {
 	return []*mss.MailTemplate{}
-}
-
-// 删除邮件模板
-func (this *MssRepo) DeleteMailTemplate(mchId, id int) error {
-	return nil
 }
 
 // 加入到发送对列
@@ -110,7 +126,7 @@ func (this *MssRepo) SaveMessage(v *mss.Message) (int32, error) {
 	} else {
 		var id int64
 		_, id, err = this._conn.GetOrm().Save(nil, v)
-		v.Id = int(id)
+		v.Id = int32(id)
 	}
 	return v.Id, err
 }
@@ -124,7 +140,7 @@ func (this *MssRepo) GetMessage(id int32) *mss.Message {
 
 // 保存用户消息关联
 func (this *MssRepo) SaveUserMsg(v *mss.To) (int32, error) {
-	return orm.I32(orm.Save(this._conn.GetOrm(), v, v.Id))
+	return orm.I32(orm.Save(this._conn.GetOrm(), v, int(v.Id)))
 }
 
 // 保存消息内容
@@ -135,7 +151,7 @@ func (this *MssRepo) SaveMsgContent(v *mss.Content) (int32, error) {
 	} else {
 		var id int64
 		_, id, err = this._conn.GetOrm().Save(nil, v)
-		v.Id = int(id)
+		v.Id = int32(id)
 	}
 	return v.Id, err
 }
@@ -209,7 +225,8 @@ func TestMessageManagerImpl_SendMessage(t *testing.T) {
 		// 是否只能阅读
 		Readonly: 1,
 	}
-	pv := mss.PhoneMessage("您好短信{Name}")
+	pv := mss.Message{
+	}
 	msg = mgr.CreateMessage(v, &pv)
 	if msg.GetDomainId() == 0 {
 		_, err = msg.Save()
