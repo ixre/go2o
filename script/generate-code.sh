0600cfff478043511a4755406a70c27e18b3b437
jarrysix <jarrysix@gmail.com>
2020-09-07 10:51:58 +0800

    protobuf

--- script/generate-code.sh
+++ script/generate-code.sh
@@ -6,19 +6,15 @@ rm -rf api_docs && \
 apidoc -i $(find . -name 'apidoc.json' -print -quit|xargs dirname) -o api_docs/
 
 
-# 生成服务代码
-# author : liuming
-# data   : 2020-09-03 11:00
-
-
 if [[ $GO2O_JAVA_HOME != "" ]];then java_target_path=$GO2O_JAVA_HOME;fi;
 
-thrift_path=$(find . -name "service.thrift" -print -quit)
 
-cmd=$1
+#cmd=$1
 
-rm -rf ./go2o/core/service/proto
-protoc -I ./proto  --go_out=plugins=grpc:proto ./proto/*.proto
+PROTO_PATH=$(find . -name "idl" -print -quit)
+TARGET_PATH=$PROTO_PATH/../proto
+mkdir -p "$TARGET_PATH"
+protoc -I "$PROTO_PATH"  --go_out=plugins=grpc:"$TARGET_PATH" "$PROTO_PATH"/*.proto
 
 
 #if [[ ${cmd} = "all" ]] || [[ ${cmd} = "format" ]];then
