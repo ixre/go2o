dc5df7f80622c486adc8512a1fa67b66a531419a
jarrysix <jarrysix@gmail.com>
2020-11-05 11:53:13 +0800

    add hfb 新增汇付快捷支付接口

---
+++ core/infrastructure/qpay/hfb/hfb.go
@@ -0,0 +1,12 @@
+package hfb
+
+/**
+ * Copyright (C) 2007-2020 56X.NET,All rights reserved.
+ *
+ * name : hfb.go
+ * author : jarrysix (jarrysix#gmail.com)
+ * date : 2020-11-05 09:27
+ * description :
+ * history :
+ */
+
