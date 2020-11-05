184df7ba169400450b0265829ff30b0e1988c1de
jarrysix <jarrysix@gmail.com>
2020-11-05 18:09:33 +0800

    添加快捷支付服务定义

--- core/infrastructure/qpay/hfb/hfb.go
+++ core/infrastructure/qpay/hfb/hfb.go
@@ -1,5 +1,12 @@
 package hfb
 
+// 快捷（银行侧)
+// http://dev.heepay.com/docs/#/KJJK?id=%e5%bf%ab%e6%8d%b7%ef%bc%88%e9%93%b6%e8%a1%8c%e4%be%a7%ef%bc%89
+
+
+// 快捷支付
+//
+
 /**
  * Copyright (C) 2007-2020 56X.NET,All rights reserved.
  *
