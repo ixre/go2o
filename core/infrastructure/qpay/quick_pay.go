3b7871a0a4193802321dd2165d74b602b3d00aee
jarrysix <jarrysix@gmail.com>
2020-11-06 12:01:44 +0800

    quick pay

---
+++ core/infrastructure/qpay/quick_pay.go
@@ -0,0 +1,102 @@
+package qpay
+
+/**
+ * Copyright (C) 2007-2020 56X.NET,All rights reserved.
+ *
+ * name : quick_pay.go
+ * author : jarrysix (jarrysix#gmail.com)
+ * date : 2020-11-05 19:51
+ * description :
+ * history :
+ */
+
+type QuickPayProvider interface {
+	// 查询银行卡信息
+	QueryCardBin(bankCardNo string) *CardBinQueryResult
+	// 检查签名是否正确
+	CheckSign(params map[string]string, signType string, sign string) bool
+	// 申请银行侧认证授权(某些银行需跳转到银行页面进行授权)
+	RequestBankSideAuth(nonce string, bankCardNo string, accountName string,
+		idCardNo string, mobile string) (*BankAuthResult, error)
+	// 根据随机ID查询银行认证状态(提供给外部查询)
+	QueryBankAuthByNonceId(id string) (*BankAuthQueryResponse, error)
+	// 根据银行卡查询银行认证状态(提供给内部查询)
+	QueryBankAuth(bankCardNo string) (*BankAuthQueryResponse, error)
+	// 直接支付
+	DirectPayment(orderNo string, fee int32, subject string, bankToken string,
+		tradeIp string, notifyUrl string, returnUrl string) (*QPaymentResponse, error)
+	// 批量付款
+	BatchTransfer(batchTradeNo string, batchTradeFee int32,
+		list []*CardTransferReq, nonce string,
+		tradeIp string, notifyUrl string) (*BatchTransferResponse,error)
+}
+
+// 银行卡查询结果
+type CardBinQueryResult struct {
+	// 错误信息
+	ErrMsg string `protobuf:"bytes,1,opt,name=ErrMsg,proto3" json:"ErrMsg,omitempty"`
+	// 银行名称
+	BankName string `protobuf:"bytes,1,opt,name=BankName,proto3" json:"BankName,omitempty"`
+	// 用户银行卡号
+	BankCardNo string `protobuf:"bytes,2,opt,name=BankCardNo,proto3" json:"BankCardNo,omitempty"`
+	// 返回的银行代号
+	BankCode string `protobuf:"bytes,3,opt,name=BankCode,proto3" json:"BankCode,omitempty"`
+	// 银行卡类型（0=储蓄卡,1=信用卡）
+	CardType int32 `protobuf:"varint,4,opt,name=CardType,proto3" json:"CardType,omitempty"`
+	// 是否需要银行端授权,如果否,则直接使用短信既可授权
+	RequireBankSideAuth  bool     `protobuf:"varint,6,opt,name=RequireBankSideAuth,proto3" json:"RequireBankSideAuth,omitempty"`
+}
+
+// 申请认证返回结果,通常直接使用返回的表单提交获取银行的授权, 并再查询授权
+type BankAuthResult struct {
+	// 随机Id
+	NonceId string `protobuf:"bytes,1,opt,name=NonceId,proto3" json:"NonceId,omitempty"`
+	// 认证的Form表单
+	AuthForm string `protobuf:"bytes,3,opt,name=AuthForm,proto3" json:"AuthForm,omitempty"`
+	// 认证需要的其他数据
+	AuthData map[string]string `protobuf:"bytes,4,rep,name=AuthData,proto3" json:"AuthData,omitempty" protobuf_key:"bytes,1,opt,name=key,proto3" protobuf_val:"bytes,2,opt,name=value,proto3"`
+}
+
+// 银行授权查询响应
+type BankAuthQueryResponse struct {
+	// 状态码，0表示成功
+	Code string `protobuf:"bytes,1,opt,name=Code,proto3" json:"Code,omitempty"`
+	// 银行授权认证返回的Token,未处理授权时为空
+	BankAuthToken string `protobuf:"bytes,3,opt,name=BankAuthToken,proto3" json:"BankAuthToken,omitempty"`
+}
+
+// 支付申请响应
+type QPaymentResponse struct {
+	// 状态码，0表示成功
+	Code string `protobuf:"bytes,1,opt,name=Code,proto3" json:"Code,omitempty"`
+	// 第三方支单据号
+	BillNo string `protobuf:"bytes,3,opt,name=BillNo,proto3" json:"BillNo,omitempty"`
+}
+
+// 付款请求
+type CardTransferReq struct {
+	// 商户订单号
+	OrderNo string `protobuf:"bytes,1,opt,name=OrderNo,proto3" json:"OrderNo,omitempty"`
+	// 银行编号
+	BankCode string `protobuf:"bytes,2,opt,name=BankCode,proto3" json:"BankCode,omitempty"`
+	// 是否为对公转账
+	TransferToCompany bool `protobuf:"varint,3,opt,name=TransferToCompany,proto3" json:"TransferToCompany,omitempty"`
+	// 付款金额,实际金额*100,无小数
+	TradeFee int32 `protobuf:"varint,4,opt,name=TradeFee,proto3" json:"TradeFee,omitempty"`
+	// 付款事由
+	Subject string `protobuf:"bytes,5,opt,name=Subject,proto3" json:"Subject,omitempty"`
+	// 省份
+	Province string `protobuf:"bytes,6,opt,name=Province,proto3" json:"Province,omitempty"`
+	// 城市
+	City string `protobuf:"bytes,7,opt,name=City,proto3" json:"City,omitempty"`
+	// 支行信息
+	StoreName string `protobuf:"bytes,8,opt,name=StoreName,proto3" json:"StoreName,omitempty"`
+}
+
+// 批量付款响应
+type BatchTransferResponse struct {
+	// 状态码，0表示成功
+	Code string `protobuf:"bytes,1,opt,name=Code,proto3" json:"Code,omitempty"`
+	// 随机ID
+	NonceId string `protobuf:"bytes,3,opt,name=NonceId,proto3" json:"NonceId,omitempty"`
+}
--- core/service/impl/quick_pay_service.go
+++ core/service/impl/quick_pay_service.go
@@ -2,11 +2,14 @@ package impl
 
 import (
 	"context"
+	"github.com/ixre/gof/crypto"
 	"github.com/ixre/gof/storage"
+	"github.com/ixre/gof/util"
 	"go2o/core/domain/interface/registry"
 	"go2o/core/infrastructure/qpay"
 	"go2o/core/infrastructure/qpay/hfb"
 	"go2o/core/service/proto"
+	"strings"
 )
 
 /**
@@ -51,32 +54,170 @@ func (q quickPayServiceImpl) QueryCardBin(_ context.Context, no *proto.BankCardN
 	}, nil
 }
 
-func (q quickPayServiceImpl) CheckSign(context context.Context, request *proto.CheckQPaySignRequest) (*proto.CheckQPaySignResponse, error) {
-	panic("implement me")
+func (q quickPayServiceImpl) CheckSign(_ context.Context, r *proto.CheckQPaySignRequest) (*proto.CheckQPaySignResponse, error) {
+	b := q.qp.CheckSign(r.Params, r.SignType, r.Sign)
+	return &proto.CheckQPaySignResponse{
+		SignOk: b,
+	}, nil
 }
 
-func (q quickPayServiceImpl) RequestBankSideAuth(context context.Context, request *proto.BankAuthRequest) (*proto.BankAuthResponse, error) {
-	panic("implement me")
+func (q quickPayServiceImpl) RequestBankSideAuth(_ context.Context, r *proto.BankAuthRequest) (*proto.BankAuthResponse, error) {
+	ret, err := q.qp.RequestBankSideAuth(r.NonceId, r.BankCardNo, r.BankAccountName,
+		r.IdCardNo, r.Mobile)
+	rsp := &proto.BankAuthResponse{
+		NonceId: r.NonceId,
+	}
+	if err != nil {
+		rsp.ErrMsg = err.Error()
+	} else {
+		rsp.AuthForm = ret.AuthForm
+		rsp.AuthData = ret.AuthData
+	}
+	return rsp, nil
 }
 
-func (q quickPayServiceImpl) QueryBankAuthResult(context context.Context, request *proto.BankAuthQueryRequest) (*proto.BankAuthQueryResponse, error) {
-	panic("implement me")
+func (q quickPayServiceImpl) QueryBankAuthResult(_ context.Context, r *proto.BankAuthQueryRequest) (*proto.BankAuthQueryResponse, error) {
+	var result *qpay.BankAuthQueryResponse
+	var err error
+	if len(r.NonceId) > 0 {
+		result, err = q.qp.QueryBankAuthByNonceId(r.NonceId)
+	} else {
+		result, err = q.qp.QueryBankAuth(r.BankCardNo)
+	}
+	if err != nil {
+		return &proto.BankAuthQueryResponse{
+			Code:          result.Code,
+			ErrMsg:        err.Error(),
+			BankAuthToken: "",
+		}, nil
+	}
+	return &proto.BankAuthQueryResponse{
+		Code:          result.Code,
+		BankAuthToken: result.BankAuthToken,
+	}, nil
 }
 
-func (q quickPayServiceImpl) DirectPayment(context context.Context, request *proto.QPaymentRequest) (*proto.QPaymentResponse, error) {
-	panic("implement me")
+func (q quickPayServiceImpl) DirectPayment(_ context.Context, r *proto.QPaymentRequest) (*proto.QPaymentResponse, error) {
+	if len(strings.TrimSpace(r.Nonce)) == 0 {
+		return &proto.QPaymentResponse{
+			Code:    "2",
+			ErrMsg:  "缺少参数:nonce",
+			NonceId: r.Nonce,
+		}, nil
+	}
+	if !q.checkSafeSign(r.Nonce, r.SafeSign) {
+		return &proto.QPaymentResponse{
+			Code:    "3",
+			ErrMsg:  "请求签名不正确",
+			NonceId: r.Nonce,
+		}, nil
+	}
+	if r.TradeUserIp == "" {
+		return &proto.QPaymentResponse{
+			Code:    "4",
+			ErrMsg:  "交易IP为空",
+			NonceId: r.Nonce,
+		}, nil
+	}
+	rsp, err := q.qp.DirectPayment(r.OrderNo, r.TradeFee, r.Subject,
+		r.BankAuthToken, r.TradeUserIp, r.ReturnUrl, r.NotifyUrl)
+	if err != nil {
+		return &proto.QPaymentResponse{
+			Code:    "1",
+			ErrMsg:  err.Error(),
+			NonceId: r.Nonce,
+		}, nil
+	}
+	return &proto.QPaymentResponse{
+		Code:    "0",
+		BillNo:  rsp.BillNo,
+		NonceId: r.Nonce,
+	}, nil
 }
 
-func (q quickPayServiceImpl) BatchTransfer(context context.Context, request *proto.BatchTransferRequest) (*proto.BatchTransferResponse, error) {
-	panic("implement me")
+func (q quickPayServiceImpl) BatchTransfer(_ context.Context, r *proto.BatchTransferRequest) (*proto.BatchTransferResponse, error) {
+	if len(strings.TrimSpace(r.Nonce)) == 0 {
+		return &proto.BatchTransferResponse{
+			Code:    "2",
+			ErrMsg:  "缺少参数:nonce",
+			NonceId: r.Nonce,
+		}, nil
+	}
+	if !q.checkSafeSign(r.Nonce, r.SafeSign) {
+		return &proto.BatchTransferResponse{
+			Code:    "3",
+			ErrMsg:  "请求签名不正确",
+			NonceId: r.Nonce,
+		}, nil
+	}
+	if r.TradeUserIp == "" {
+		return &proto.BatchTransferResponse{
+			Code:    "4",
+			ErrMsg:  "交易IP为空",
+			NonceId: r.Nonce,
+		}, nil
+	}
+	batchList := q.parseBatchList(r.BatchList)
+	ret, err := q.qp.BatchTransfer(r.BatchTradeNo, r.BatchTradeFee,
+		batchList, r.Nonce, r.TradeUserIp, r.NotifyUrl)
+	if err != nil {
+		return &proto.BatchTransferResponse{
+			Code:    "1",
+			ErrMsg:  err.Error(),
+			NonceId: r.Nonce,
+		}, nil
+	}
+	return &proto.BatchTransferResponse{
+		Code:    ret.Code,
+		ErrMsg:  err.Error(),
+		NonceId: r.Nonce,
+	}, nil
+}
+
+// 检查安全请求签名,lowercase(md5(nonce+secret))
+func (q quickPayServiceImpl) checkSafeSign(nonce string, sign string) bool {
+	secret, err := q.registryRepo.GetValue("qp_safe_secret")
+	if err != nil {
+		println("[ Go2o][ Warning]: ", err.Error(), " key: qp_safe_secret")
+		return false
+	}
+	if len(secret) == 0 {
+		println("[ Go2o][ Warning]: quick payment safe secret not set")
+	}
+	md5 := crypto.Md5([]byte(nonce + secret))
+	return strings.ToLower(md5) == sign
+}
+
+func (q quickPayServiceImpl) parseBatchList(list []*proto.CardTransferRequest) []*qpay.CardTransferReq {
+	dst := make([]*qpay.CardTransferReq, len(list))
+	for i, v := range list {
+		dst[i] = &qpay.CardTransferReq{
+			OrderNo:           v.OrderNo,
+			BankCode:          v.BankCode,
+			TransferToCompany: v.TransferToCompany,
+			TradeFee:          v.TradeFee,
+			Subject:           v.Subject,
+			Province:          v.Province,
+			City:              v.City,
+			StoreName:         v.StoreName,
+		}
+	}
+	return dst
 }
 
 func initQPayConfig(repo registry.IRegistryRepo) {
+	// 快捷支付安全签名密钥
+	if _, err := repo.GetValue("qp_safe_secret"); err != nil {
+		l := util.RandString(10)
+		token := strings.ToLower(crypto.Md5([]byte(l)))
+		repo.CreateUserKey("qp_safe_secret", token[8:26], "快捷支付安全签名密钥")
+	}
 	// 初始化HFB
-	if _,err := repo.GetValue("qp_hfb_agent_id");err != nil {
+	if _, err := repo.GetValue("qp_hfb_agent_id"); err != nil {
 		repo.CreateUserKey("qp_hfb_agent_id", "0000000", "汇付宝(快捷支付)商户编号")
 	}
-	if _,err := repo.GetValue("qp_hfb_md5_key");err != nil{
+	if _, err := repo.GetValue("qp_hfb_md5_key"); err != nil {
 		repo.CreateUserKey("qp_hfb_md5_key", "CC08C5E3E69F4E6B85F1DC0B", "汇付宝(快捷支付)签名KEY(md5)")
 	}
+
 }
