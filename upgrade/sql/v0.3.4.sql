3c1689cb9dbd9c67c2b3e3a0ac66fcfb3f61c24f
jarrysix <jarrysix@gmail.com>
2022-11-29 09:58:22 +0800

    v0.3.4.sql

--- upgrade/sql/v0.3.4.sql
+++ upgrade/sql/v0.3.4.sql
@@ -4,3 +4,8 @@ ALTER TABLE IF EXISTS public.perm_dept
 
 COMMENT ON COLUMN public.perm_dept.code
     IS '部门代码';
+
+ALTER TABLE public.order_list ALTER COLUMN id TYPE bigint;
+
+ALTER TABLE public.order_list
+    ALTER COLUMN subject TYPE character varying(40) COLLATE pg_catalog."default";
