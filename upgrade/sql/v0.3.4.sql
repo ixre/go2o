938c96e80c35e03c9392b945ca8119abbf6380d6
jarrysix <jarrysix@gmail.com>
2022-12-31 09:50:45 +0800

    feat: user flag request table

--- upgrade/sql/v0.3.4.sql
+++ upgrade/sql/v0.3.4.sql
@@ -48,3 +48,31 @@ ALTER TABLE "public".article_list
 
 CREATE INDEX mm_account_member_id_wallet_code 
   ON "public".mm_account (wallet_code, member_id);
+
+
+/** 2022-12-29 12:31 */
+  CREATE TABLE mm_flag_request (
+  id               BIGSERIAL NOT NULL, 
+  member_id       int8 NOT NULL, 
+  request_flag    int4 NOT NULL, 
+  contact_name    varchar(20) NOT NULL, 
+  contact_phone   varchar(20) NOT NULL, 
+  contact_address varchar(120) NOT NULL, 
+  audit_state     int4 NOT NULL, 
+  audit_time      int8 NOT NULL, 
+  audit_uid       int8 NOT NULL, 
+  audit_remark    varchar(120) NOT NULL, 
+  create_time     int8 NOT NULL, 
+  PRIMARY KEY (id));
+COMMENT ON TABLE mm_flag_request IS '会员申请标志请求';
+COMMENT ON COLUMN mm_flag_request.id IS '编号';
+COMMENT ON COLUMN mm_flag_request.member_id IS '会员编号';
+COMMENT ON COLUMN mm_flag_request.request_flag IS '申请标志';
+COMMENT ON COLUMN mm_flag_request.contact_name IS '联系人';
+COMMENT ON COLUMN mm_flag_request.contact_phone IS '联系电话';
+COMMENT ON COLUMN mm_flag_request.contact_address IS '联系地址';
+COMMENT ON COLUMN mm_flag_request.audit_state IS '审核状态';
+COMMENT ON COLUMN mm_flag_request.audit_time IS '审核时间';
+COMMENT ON COLUMN mm_flag_request.audit_uid IS '审核人';
+COMMENT ON COLUMN mm_flag_request.audit_remark IS '审核意见';
+COMMENT ON COLUMN mm_flag_request.create_time IS '创建时间';
