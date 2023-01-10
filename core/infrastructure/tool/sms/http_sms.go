5185d4843816d5c362801cd7d3cfe072ab137370
jarrysix <jarrysix@gmail.com>
2023-01-10 16:11:21 +0800

    fix: replace char variable to number variable

---
+++ core/infrastructure/tool/sms/http_sms.go
@@ -0,0 +1,123 @@
+package sms
+
+import (
+	"crypto/tls"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/ixre/gof/types/typeconv"
+	"github.com/ixre/gof/util"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/encoding/traditionalchinese"
+)
+
+// 通过HTTP-API发送短信, 短信模板参数在data里指定
+func sendPhoneMsgByHttpApi(api *SmsApi, phone, content string, data []string, templateId string) error {
+	//如果指定了编码，则先编码内容
+	if api.Charset != "" {
+		dst, err := EncodingTransform([]byte(content), api.Charset)
+		if err != nil {
+			return err
+		}
+		content = string(dst)
+	}
+	// 如果GET发送,需要编码
+	if api.Method == "GET" {
+		content = url.QueryEscape(content)
+	}
+	// 请求参数
+	params := map[string]string{
+		"key":          api.Key,
+		"secret":       api.Secret,
+		"phone":        phone,
+		"content":      content,
+		"templateId":   templateId,
+		"templateData": strings.Join(data, ","),
+		"stamp":        fmt.Sprintf("%s%d", util.RandString(3), time.Now().Unix()),
+	}
+	body := resolveApiRequestParams(api.Params, params)
+
+	// 创建请求
+	req, err := createHttpRequest(api, body)
+	if err != nil {
+		return err
+	}
+	cli := &http.Client{}
+	// 忽略证书
+	if req.TLS != nil || (len(api.ApiUrl) >= 8 && api.ApiUrl[:8] == "https://") {
+		cli.Transport = &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+	}
+	// 读取响应
+	rsp, err := cli.Do(req)
+	if err == nil {
+		defer rsp.Body.Close()
+		if rsp.StatusCode != http.StatusOK {
+			return fmt.Errorf("error : %d", rsp.StatusCode)
+		}
+		//log.Println("[ Go2o][ Sms]:", body)
+		var data []byte
+		data, err = io.ReadAll(rsp.Body)
+		if err == nil {
+			result := string(data)
+			if !strings.Contains(result, api.SuccessChar) {
+				return errors.New("send fail : " + result + " message body:" + content)
+			}
+		}
+	}
+	return err
+}
+
+// 解析HTTP短信中的请求参数
+func resolveApiRequestParams(params string, data map[string]string) string {
+	for k, v := range data {
+		str, _ := typeconv.String(v)
+		params = strings.Replace(params, "{"+k+"}",
+			str, -1)
+	}
+	return params
+}
+
+// 创建HTTP短信发送请求
+func createHttpRequest(api *SmsApi, body string) (*http.Request, error) {
+	var req *http.Request
+	var err error
+	if api.Method == "POST" {
+		req, err = http.NewRequest(api.Method, api.ApiUrl, strings.NewReader(body))
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	} else {
+		url := api.ApiUrl
+		if strings.Contains(api.ApiUrl, "?") {
+			url += "&"
+		} else {
+			url += "?"
+		}
+		req, err = http.NewRequest(api.Method, url+body, nil)
+	}
+	return req, err
+}
+
+// 编码
+func EncodingTransform(src []byte, enc string) ([]byte, error) {
+	var ec encoding.Encoding
+	switch enc {
+	default:
+		return src, nil
+	case "GBK":
+		ec = simplifiedchinese.GBK
+	case "GB2312":
+		ec = simplifiedchinese.HZGB2312
+	case "BIG5":
+		ec = traditionalchinese.Big5
+	}
+	dst := make([]byte, len(src)*2)
+	n, _, err := ec.NewEncoder().Transform(dst, src, true)
+	return dst[:n], err
+}
