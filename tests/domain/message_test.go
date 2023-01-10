1d4313167594542e30f131d945ae81174fcc304e
jarrysix <jarrysix@gmail.com>
2023-01-10 16:11:15 +0800

    fix: replace char variable to number variable

--- tests/domain/message_test.go
+++ tests/domain/message_test.go
@@ -12,11 +12,11 @@ func TestSendPhoneMessage(t *testing.T) {
 	templatId := ""
 	manager := ti.Factory.GetMssRepo().NotifyManager()
 	err := manager.SendPhoneMessage("13162222872",
-		notify.PhoneMessage("测试短信消息"),
-		[]string{"注册账户", "jarrysix"},
+		notify.PhoneMessage("测试短信:你本次进行{action}的验证码为{1},有效期为:{minites}"),
+		[]string{"注册账户", "3101", "30"},
 		templatId,
 	)
-	if err != nil{
+	if err != nil {
 		t.Error(err)
 	}
 	time.Sleep(3000)
