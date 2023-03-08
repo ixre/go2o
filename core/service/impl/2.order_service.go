97c3bee288223b848cdc256f7be1173f95b52606
jarrysix <jarrysix@gmail.com>
2023-03-08 16:56:44 +0800

    feat: prepare order empty order

--- core/service/impl/2.order_service.go
+++ core/service/impl/2.order_service.go
@@ -153,10 +153,7 @@ func (s *orderServiceImpl) SubmitOrder(_ context.Context, r *proto.SubmitOrderRe
 func (s *orderServiceImpl) PrepareOrder(_ context.Context, r *proto.PrepareOrderRequest) (*proto.PrepareOrderResponse, error) {
 	ic := s.getShoppingCart(r.BuyerId, r.CartCode)
 	if ic == nil {
-		return &proto.PrepareOrderResponse{
-			ErrCode: 2,
-			ErrMsg:  cart.ErrNoSuchCart.Error(),
-		}, nil
+		ic = s.cartRepo.NewTempNormalCart(int(r.BuyerId), r.CartCode)
 	}
 	if r.Item != nil {
 		err := ic.Put(r.Item.ItemId, r.Item.SkuId, r.Item.Quantity, true, true)
