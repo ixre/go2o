38a81b98c63e9d56e71a1dcf71ff5ef0b68064a1
jarrysix <jarrysix@gmail.com>
2024-12-24 10:12:31 +0800

    bridge.html

--- assets/assets.go
+++ assets/assets.go
@@ -5,5 +5,12 @@ import _ "embed"
 //go:embed sensitive_dict.txt
 var SensitiveDict []byte
 
+// 下载页面
+
 //go:embed app.html
 var AppDownHtml string
+
+// 桥接页面,用于H5跳转
+//
+//go:embed bridge.html
+var BridgeHtml string
